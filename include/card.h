79af173d62f7d04f1acd68f68ceda920c49f7a0e
Pheenoh <pheenoh@gmail.com>
2026-08-30 16:47:04 -0600

    Decompile the card state setters and deck accessors

---
+++ include/card.h
@@ -0,0 +1,36 @@
+#ifndef GUARD_CARD_H
+#define GUARD_CARD_H
+
+#include "types.h"
+
+#define CARD_ID_MASK 0xFFF
+#define DECK_SIZE 98
+
+typedef struct CardDef {
+    u8 unk_00[0x2A];
+    u8 unk_2A;
+    u8 unk_2B[0x09];
+} CardDef;
+
+#define gCardDefs ((CardDef*)0x08F70AB0)
+#define gCardCollection ((u16*)0x0203A080)
+#define gCardCount (*(u16*)0x0203A858)
+#define gDecks ((u8*)0x02039DE0)
+#define gActiveDeck (*(u8*)0x02034AB0)
+#define gUnk_02034AA4 (*(u32*)0x02034AA4)
+
+void func_0807E1F4(void);
+void func_0807E200(void);
+void func_0807E20C(void);
+void func_0807E218(void);
+void func_0807E224(void);
+void func_0807E230(void);
+void func_0807E23C(void);
+void func_0807E248(void);
+void func_0807E254(void);
+
+u8* func_08085740(void);
+u8 func_08085B84(u16 index);
+u8 func_08085FA4(void);
+
+#endif /* GUARD_CARD_H */
