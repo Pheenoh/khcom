72227fd8a44b0722cb9cb8344f5c6de4a9ef30dd
Pheenoh <pheenoh@gmail.com>
2026-08-30 21:58:36 -0600

    Decompile the small tier of bos6.c

---
+++ include/bos6.h
@@ -0,0 +1,22 @@
+#ifndef GUARD_BOS6_H
+#define GUARD_BOS6_H
+
+#include "types.h"
+#include "game.h"
+
+void func_0810A018(void* a);
+void func_0810A498(void* a);
+void func_0801BCD4(void* a);
+
+extern void* gUnk_02036010;
+
+void* func_0810B49C(void);
+void func_0810B4A8(u8* p);
+void func_0810B51C(u8* p);
+void func_0810BF24(u8** p, u8 v);
+void func_0810C2C4(u8** p);
+void func_0810C2CC(u8* p, u16 a, u16 b);
+void func_0810C2E0(u8* work);
+void task_bos_pc_2(void* work);
+
+#endif /* GUARD_BOS6_H */
