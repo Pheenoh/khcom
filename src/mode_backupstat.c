0929f803c1a2c78155fa1c1d299ea953e9f6e2cf
Pheenoh <pheenoh@gmail.com>
2026-08-31 14:14:28 -0600

    Decompile most of mode_backupstat and mode_worldwarp

--- src/mode_backupstat.c
+++ src/mode_backupstat.c
@@ -53,6 +53,7 @@ void mode_backupstat_0(void) {
         func_0805FCB0(120, i * 9, 2, gUnk_09EF9668[gUnk_02035FF0[i]]);
     }
 }
+
 void func_081097F4(void) {
     u16 slot;
 
@@ -79,6 +80,7 @@ void func_081097F4(void) {
     }
     func_0805FCB0(120, gUnk_02035FE8 * 9, 2, gUnk_09EF9668[gUnk_02035FF0[gUnk_02035FE8]]);
 }
+
 void mode_backupstat_1(void) {
     u8 prev;
 
@@ -207,7 +209,110 @@ u16 func_0810A000(PcWork* work, s32 a, s32 b) {
     return (0xEFFC - ((a >> 8) << 2)) | b;
 }
 
+#ifdef NON_MATCHING
+void func_0810A018(PcWork* work) {
+    PcAnimStep* step;
+    PcSpriteCmd* cmd;
+    PcSpriteDef* def;
+    PcOam* oam;
+    void* gfx;
+    s16 sx;
+    s16 sy;
+    s16 ox;
+    s16 oy;
+    s32 i;
+    s32 j;
+    u16 t;
+
+    if (work->unk_02C == 0) {
+        return;
+    }
+    step = &work->unk_02C[work->unk_032];
+    if (step->unk_00 != 0) {
+        return;
+    }
+    ox = 0;
+    oy = 0;
+    if (work->unk_2E8.unk_05 == 1) {
+        ox = gUnk_02039DC8->unk_68;
+        oy = gUnk_02039DC8->unk_6A;
+    }
+    gfx = work->unk_04C;
+    if (work->unk_018 == 0) {
+        if (func_0801CA00(&work->unk_054)) {
+            gfx = work->unk_050;
+            work->unk_00A = 1;
+        } else {
+            work->unk_00A = 0;
+        }
+    } else {
+        work->unk_00A = 0;
+    }
+    if (work->unk_00A != work->unk_00C) {
+        if (work->unk_00A == 0) {
+            LoadPalette(gUnk_09D69274, gUnk_05000000, 32);
+        } else {
+            LoadPalette(gUnk_08F69BC4, gUnk_05000000, 32);
+        }
+        work->unk_00C = work->unk_00A;
+    }
+    WorldToScreen(&sx, &sy, work->unk_020 - ox * 256, work->unk_024 - oy * 256, work->unk_028);
+    cmd = gUnk_09EF9C34[step->unk_20];
+
+    for (i = 23; i >= 0; i--) {
+        work->unk_2FC[i].count = 0;
+    }
+
+    j = 0;
+    while (!(cmd->unk_00 & 0x80)) {
+        if (cmd->unk_00 & 1) {
+            DrawSprite(cmd->unk_04 + sx, cmd->unk_06 + sy, gUnk_09EFAB18[cmd->unk_01],
+                work->unk_044[j], gfx, 0,
+                func_08109FF0(work, work->unk_024 + (cmd->unk_02 << 10) - 0x3300),
+                func_0810A000(work, work->unk_024 + (cmd->unk_02 << 10) - 0x3300, 1));
+        } else {
+            def = gUnk_09EFBB18[cmd->unk_01];
+            oam = &work->unk_2FC[cmd->unk_02];
+            t = (u8)def->unk_02;
+            if (t & 0x80) {
+                t |= 0xFF00;
+            }
+            if ((u16)((s16)((s16)t + cmd->unk_06) + sy + 7) <= 0xAE) {
+                oam->attr[oam->count * 3 + 1] = (def->unk_02 & 0xFF00) | ((cmd->unk_06 + def->unk_02 + 0x40) & 0xFF);
+                oam->attr[oam->count * 3 + 2] = (def->unk_04 & 0xFE00) | ((def->unk_04 + cmd->unk_04) & 0x1FF);
+                oam->attr[oam->count * 3 + 3] = def->unk_06;
+                oam->count++;
+            }
+        }
+        cmd++;
+        j++;
+    }
+
+    for (i = 23; i >= 0; i--) {
+        if (work->unk_2FC[i].count != 0) {
+            work->unk_2FC[i].attr[0] = work->unk_2FC[i].count;
+            DrawSprite(sx, sy - 0x40, work->unk_2FC[i].attr, work->unk_040, gfx, 0,
+                func_08109FF0(work, work->unk_024 + i * 0x400 - 0x3400),
+                func_0810A000(work, work->unk_024 + i * 0x400 - 0x3400, 1));
+        }
+    }
+
+    if (work->unk_02C == gUnk_09A4C278) {
+        gUnk_02039B84->unk_0D8 = 24;
+    } else {
+        gUnk_02039B84->unk_0D8 = 0xFFF6;
+    }
+    WorldToScreen(&sx, &sy, work->unk_020 + ((-0x70 - ox) << 8), work->unk_024 + ((-0x64 - oy) << 8), work->unk_028);
+    if (work->unk_032 != work->unk_036) {
+        LoadBgTiles(1, gUnk_09A4AC84[step->unk_1E].unk_00, gUnk_09A4AC84[step->unk_1E].unk_04);
+        LoadBgMap(1, gUnk_09A4AC84[step->unk_1E].unk_08, gUnk_09A4AC84[step->unk_1E].unk_0C);
+        work->unk_036 = work->unk_032;
+    }
+    SetBgScroll(1, 0x50 - sx, 8 - sy);
+}
+#else
 INCLUDE_ASM("mode_backupstat/func_0810A018.s");
+#endif
 
 u8 func_0810A424(PcWork* work) {
     PcAnimStep* step;
