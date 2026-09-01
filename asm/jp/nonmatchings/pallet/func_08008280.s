147f33f5d02b67b6bd2ba5693e596a31889ef3ef
Pheenoh <pheenoh@gmail.com>
2026-08-31 19:30:40 -0600

    Decompile pallet.c in full

--- asm/jp/nonmatchings/pallet/func_08008280.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_08008280
-	.thumb
-	.thumb_func
-	.type func_08008280, %function
-func_08008280:
-	.incbin "roms/B8CJ.gba", 0x8280, 0xbc
-.syntax divided
