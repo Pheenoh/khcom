53b7e7f58da18fb6f1e25acbd4456db3a7cf8c37
Pheenoh <pheenoh@gmail.com>
2026-08-31 19:11:16 -0600

    Decompile 3 more engine.c functions; finish Dma3Queue

--- asm/jp/nonmatchings/engine/func_080057A0.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_080057A0
-	.thumb
-	.thumb_func
-	.type func_080057A0, %function
-func_080057A0:
-	.incbin "roms/B8CJ.gba", 0x57a0, 0x70
-.syntax divided
