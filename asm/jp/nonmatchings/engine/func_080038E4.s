1e1403ecc59a22ed8264793bb76ae0e19aaff39c
Pheenoh <pheenoh@gmail.com>
2026-09-01 12:56:14 -0600

    engine: decompile func_080038E4

--- asm/jp/nonmatchings/engine/func_080038E4.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_080038E4
-	.thumb
-	.thumb_func
-	.type func_080038E4, %function
-func_080038E4:
-	.incbin "roms/B8CJ.gba", 0x38e4, 0x8c
-.syntax divided
