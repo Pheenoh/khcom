29ab418385a773007a995166b0c12be3664075a2
Pheenoh <pheenoh@gmail.com>
2026-09-01 09:29:50 -0600

    Name the entry function AgbMain

--- asm/jp/nonmatchings/main/AgbMain.s
+++ asm/jp/nonmatchings/main/AgbMain.s
@@ -1,9 +1,9 @@
 .syntax unified
 	.align 2, 0
-	.global func_0800044C
+	.global AgbMain
 	.thumb
 	.thumb_func
-	.type func_0800044C, %function
-func_0800044C:
+	.type AgbMain, %function
+AgbMain:
 	.incbin "roms/B8CJ.gba", 0x44c, 0x90
 .syntax divided
