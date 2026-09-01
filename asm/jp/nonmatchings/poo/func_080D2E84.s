fdbc8a7af27dcc23b28c2c113b64326d71a66193
Pheenoh <pheenoh@gmail.com>
2026-08-31 23:40:14 -0600

    Repair a corrupted symbols.txt line; teach split_func.py blob chunks

---
+++ asm/jp/nonmatchings/poo/func_080D2E84.s
@@ -0,0 +1,9 @@
+.syntax unified
+	.align 2, 0
+	.global func_080D2E84
+	.thumb
+	.thumb_func
+	.type func_080D2E84, %function
+func_080D2E84:
+	.incbin "roms/B8CJ.gba", 0xd2f38, 0x14
+.syntax divided
