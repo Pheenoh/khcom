9dcaaef3abef4028cb79175a287a7e430294e89f
Pheenoh <pheenoh@gmail.com>
2026-09-01 15:46:40 -0600

    engine: match the BG-reset trio, 29 -> 26 INCLUDE_ASM

--- asm/jp/nonmatchings/engine/func_08004E64.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_08004E64
-	.thumb
-	.thumb_func
-	.type func_08004E64, %function
-func_08004E64:
-	.incbin "roms/B8CJ.gba", 0x4e64, 0xa4
-.syntax divided
