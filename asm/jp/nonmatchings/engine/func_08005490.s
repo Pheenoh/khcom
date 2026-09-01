b6a1d04325c393a7731721db5a7a647b0527af0a
Pheenoh <pheenoh@gmail.com>
2026-08-31 18:33:09 -0600

    Decompile 9 more engine.c functions

--- asm/jp/nonmatchings/engine/func_08005490.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_08005490
-	.thumb
-	.thumb_func
-	.type func_08005490, %function
-func_08005490:
-	.incbin "roms/B8CJ.gba", 0x5490, 0x38
-.syntax divided
