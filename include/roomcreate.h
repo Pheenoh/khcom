a9acf2d7bad835125c0a72d52d02e2a54a813e84
Pheenoh <pheenoh@gmail.com>
2026-08-31 19:07:08 -0600

    Decompile roomcreate.c in full

---
+++ include/roomcreate.h
@@ -0,0 +1,106 @@
+#ifndef GUARD_ROOMCREATE_H
+#define GUARD_ROOMCREATE_H
+
+#include "types.h"
+
+typedef struct ListNode {
+    void* unk_00;
+    u8 unk_04[0x04];
+    struct ListNode* unk_08;
+    u16 unk_0C;
+} ListNode;
+
+typedef struct TaskPool {
+    ListNode head;
+    void* unk_10;
+} TaskPool;
+
+typedef struct TaskDesc {
+    const char* name;
+    void (*unk_04)(void* work, void* arg);
+    void* unk_08;
+    void (*unk_0C)(void* work);
+    void* unk_10;
+    s32 unk_14;
+} TaskDesc;
+
+typedef struct FldPos {
+    s32 unk_00;
+    s32 unk_04;
+    s32 unk_08;
+    s32 unk_0C;
+} FldPos;
+
+typedef struct FldActor {
+    FldPos unk_00;
+    s32 unk_10;
+    u8 unk_14;
+    u8 unk_15[0x3B];
+} FldActor;
+
+typedef struct UnkStruct_02039BA0 {
+    s32 unk_00;
+    s32 unk_04;
+    s32 unk_08;
+    s32 unk_0C;
+    u8 unk_10[0x08];
+    FldActor unk_18;
+    FldActor* unk_68;
+    u8 unk_6C[0x04];
+    u32 unk_70;
+} UnkStruct_02039BA0;
+
+typedef struct RoomCreateWork {
+    s32 unk_00;
+    s32 unk_04;
+    s32 unk_08;
+    s32 unk_0C;
+    s32 unk_10;
+    s32 unk_14;
+    s32 unk_18;
+    s32 unk_1C;
+    s32 unk_20;
+    u8 unk_24;
+    u8 unk_25;
+    s16 unk_26;
+    u8 unk_28;
+    u8 unk_29;
+    u8 unk_2A[0x02];
+    TaskPool unk_2C;
+    s32 unk_40;
+} RoomCreateWork;
+
+extern UnkStruct_02039BA0* gUnk_02039BA0;
+extern s16 gSineTable[];
+extern u8 gUnk_0203DB10[];
+extern TaskDesc gUnk_09EDB380;
+extern TaskDesc gUnk_09EDB398;
+extern TaskDesc gUnk_09EE7668;
+
+struct Task;
+
+struct Task* TaskCreate(TaskPool* pool, TaskDesc* desc, void* arg);
+void TaskPoolInit(TaskPool* pool, s32 count);
+void TaskPoolUpdate(TaskPool* pool);
+void TaskPoolDraw(TaskPool* pool);
+void TaskPoolDestroy(TaskPool* pool);
+void SetBgPriority(s32 bg, u16 priority);
+void func_0800501C(s32 bg);
+void func_080058FC(s32* value, s32 target, u16 steps);
+void func_08006120(s32 a, s32 b);
+void func_080062F4(u16 a, s32 b);
+void func_08093B78(TaskPool* pool, u8* p);
+void func_08093C04(void);
+u8 func_080DFD84(FldPos* p);
+void func_080E0298(s32 a, s32 b);
+void func_080E0418(void);
+void func_080E042C(void);
+void m4aSongNumStart(u16 song);
+void m4aMPlayVolumeControl(void* mplay, u16 trackBits, u16 volume);
+
+void task_roomcreate_0(RoomCreateWork* work);
+u8 task_roomcreate_1(RoomCreateWork* work);
+void task_roomcreate_2(RoomCreateWork* work);
+void task_roomcreate_3(RoomCreateWork* work);
+
+#endif /* GUARD_ROOMCREATE_H */
--- src/roomcreate.c
+++ src/roomcreate.c
@@ -1,6 +1,176 @@
 #include "macros.h"
+#include "roomcreate.h"
 
-INCLUDE_ASM("roomcreate/task_roomcreate_0.s");
-INCLUDE_ASM("roomcreate/task_roomcreate_1.s");
-INCLUDE_ASM("roomcreate/task_roomcreate_2.s");
-INCLUDE_ASM("roomcreate/task_roomcreate_3.s");
+
+void task_roomcreate_0(RoomCreateWork* work) {
+    FldActor* act;
+
+    func_08093C04();
+    gUnk_02039BA0->unk_70 |= 0x40000;
+    gUnk_02039BA0->unk_70 |= 2;
+    work->unk_29 = 0;
+    work->unk_28 = 0;
+    work->unk_26 = 0;
+    work->unk_40 = 0;
+    SetBgPriority(0, 2);
+    SetBgPriority(1, 2);
+    TaskPoolInit(&work->unk_2C, 3);
+    work->unk_00 = gUnk_02039BA0->unk_18.unk_00.unk_00;
+    work->unk_04 = gUnk_02039BA0->unk_18.unk_00.unk_04;
+    work->unk_08 = gUnk_02039BA0->unk_18.unk_00.unk_08;
+    act = gUnk_02039BA0->unk_68;
+    work->unk_0C = act->unk_00.unk_00;
+    work->unk_10 = act->unk_00.unk_04;
+    work->unk_14 = act->unk_00.unk_08;
+    work->unk_24 = act->unk_14;
+    work->unk_18 = work->unk_0C + gSineTable[work->unk_24] * 50;
+    work->unk_1C = work->unk_10 + -gSineTable[work->unk_24 + 0x40] * 50;
+    work->unk_20 = work->unk_14;
+    work->unk_25 = gUnk_02039BA0->unk_18.unk_14;
+}
+
+u8 task_roomcreate_1(RoomCreateWork* work) {
+    s16 steps;
+    s32 i;
+
+    switch (work->unk_40) {
+    case 0:
+        if (work->unk_26 == 0) {
+            gUnk_02039BA0->unk_18.unk_14 = work->unk_24 + 0x80;
+            TaskCreate(&work->unk_2C, &gUnk_09EE7668, &work->unk_28);
+            gUnk_02039BA0->unk_70 |= 0x80000;
+        }
+        steps = 30 - work->unk_26;
+        func_080058FC(&gUnk_02039BA0->unk_18.unk_00.unk_00, work->unk_18, steps);
+        func_080058FC(&gUnk_02039BA0->unk_18.unk_00.unk_04, work->unk_1C, steps);
+        func_080058FC(&gUnk_02039BA0->unk_18.unk_00.unk_08, work->unk_20, steps);
+        if (steps <= 1) {
+            func_080E0418();
+            work->unk_40 = 1;
+            gUnk_02039BA0->unk_70 &= ~0x80000;
+            work->unk_26 = 8;
+        } else {
+            func_080E0298((gUnk_02039BA0->unk_18.unk_00.unk_00 + work->unk_0C) / 2,
+                          (gUnk_02039BA0->unk_18.unk_00.unk_04 + gUnk_02039BA0->unk_18.unk_00.unk_08 + work->unk_10 + work->unk_14) / 2);
+            work->unk_26++;
+        }
+        break;
+    case 1:
+        if (work->unk_26 > 0) {
+            func_080058FC(&gUnk_02039BA0->unk_00, gUnk_02039BA0->unk_08 - 0x7800, work->unk_26);
+            func_080058FC(&gUnk_02039BA0->unk_04, gUnk_02039BA0->unk_0C - 0x6000, work->unk_26);
+            work->unk_26--;
+        } else if (work->unk_26 == 0) {
+            m4aMPlayVolumeControl(gUnk_0203DB10, 0xFF, 0x80);
+            func_08093B78(&work->unk_2C, &work->unk_29);
+            SetBgPriority(1, 1);
+            work->unk_26--;
+        }
+        switch (work->unk_29) {
+        case 1:
+            work->unk_40 = 2;
+            work->unk_26 = 0;
+            func_080E042C();
+            SetBgPriority(1, 2);
+            break;
+        case 2:
+            work->unk_40 = 3;
+            work->unk_26 = 0;
+            break;
+        }
+        break;
+    case 3:
+        if (work->unk_26 == 0) {
+            gUnk_02039BA0->unk_70 |= 0x100000;
+            func_0800501C(2);
+            func_0800501C(3);
+            func_08006120(0, 1);
+            work->unk_26++;
+        } else if (work->unk_26 == 1) {
+            for (i = 0; i <= 31; i++) {
+                func_080062F4(i, 0);
+            }
+            work->unk_26++;
+        } else if (work->unk_26 <= 19) {
+            work->unk_26++;
+        } else if (work->unk_26 == 20) {
+            m4aSongNumStart(0x77);
+            TaskCreate(&work->unk_2C, &gUnk_09EDB398, (void*)(u32)work->unk_24);
+            work->unk_26++;
+        }
+        if (work->unk_29 == 1) {
+            work->unk_26 = 0;
+            work->unk_40 = 4;
+        }
+        break;
+    case 4:
+        if (work->unk_26 == 16) {
+            TaskCreate(&work->unk_2C, &gUnk_09EDB380, (void*)(u32)work->unk_24);
+        }
+        if (work->unk_26 == 40) {
+            gUnk_02039BA0->unk_70 |= 0x200000;
+        }
+        if (work->unk_26 > 60) {
+            work->unk_26 = 0;
+            work->unk_40 = 6;
+            gUnk_02039BA0->unk_70 &= ~0x100000;
+        } else {
+            work->unk_26++;
+        }
+        break;
+    case 5:
+        if (work->unk_26 > 60) {
+            work->unk_26 = 0;
+            work->unk_40 = 6;
+        } else {
+            work->unk_26++;
+        }
+        break;
+    case 2:
+        if (work->unk_26 == 0) {
+            work->unk_28 = 1;
+            gUnk_02039BA0->unk_70 |= 0x80000;
+        }
+        steps = 30 - work->unk_26;
+        func_080058FC(&gUnk_02039BA0->unk_18.unk_00.unk_00, work->unk_00, steps);
+        func_080058FC(&gUnk_02039BA0->unk_18.unk_00.unk_04, work->unk_04, steps);
+        func_080058FC(&gUnk_02039BA0->unk_18.unk_00.unk_08, work->unk_08, steps);
+        if (steps <= 1) {
+            gUnk_02039BA0->unk_18.unk_14 = work->unk_25;
+            gUnk_02039BA0->unk_70 &= ~0x40000;
+            gUnk_02039BA0->unk_70 &= ~2;
+            func_0800501C(0);
+            SetBgPriority(1, 1);
+            return 0;
+        }
+        work->unk_26++;
+        func_080E0298((gUnk_02039BA0->unk_18.unk_00.unk_00 + work->unk_0C) / 2,
+                      (gUnk_02039BA0->unk_18.unk_00.unk_04 + gUnk_02039BA0->unk_18.unk_00.unk_08 + work->unk_10 + work->unk_14) / 2);
+        break;
+    case 6:
+        if (work->unk_26 == 0) {
+            gUnk_02039BA0->unk_70 |= 0x80000;
+        }
+        steps = 40 - work->unk_26;
+        func_080058FC(&gUnk_02039BA0->unk_18.unk_00.unk_00, work->unk_0C, steps);
+        func_080058FC(&gUnk_02039BA0->unk_18.unk_00.unk_04, work->unk_10, steps);
+        func_080058FC(&gUnk_02039BA0->unk_18.unk_00.unk_08, work->unk_14, steps);
+        if (func_080DFD84(&gUnk_02039BA0->unk_18.unk_00)) {
+            gUnk_02039BA0->unk_70 |= 0x10;
+        }
+        break;
+    case 7:
+        break;
+    }
+    TaskPoolUpdate(&work->unk_2C);
+    return 1;
+}
+
+void task_roomcreate_2(RoomCreateWork* work) {
+    TaskPoolDraw(&work->unk_2C);
+}
+
+void task_roomcreate_3(RoomCreateWork* work) {
+    m4aMPlayVolumeControl(gUnk_0203DB10, 0xFF, 0x100);
+    TaskPoolDestroy(&work->unk_2C);
+}
