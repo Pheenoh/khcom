e2396720d611b6f4778f28f3b62c3d066810b8e5
Pheenoh <pheenoh@gmail.com>
2026-08-31 19:33:51 -0600

    Decompile task.c in full

--- asm/jp/nonmatchings/task/func_0801CCB4.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_0801CCB4
-	.thumb
-	.thumb_func
-	.type func_0801CCB4, %function
-func_0801CCB4:
-	.incbin "roms/B8CJ.gba", 0x1ccb4, 0x6c
-.syntax divided
--- asm/jp/nonmatchings/task/func_0801CD8C.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_0801CD8C
-	.thumb
-	.thumb_func
-	.type func_0801CD8C, %function
-func_0801CD8C:
-	.incbin "roms/B8CJ.gba", 0x1cd8c, 0xc
-.syntax divided
