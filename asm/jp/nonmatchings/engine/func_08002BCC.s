59d01836e03cb5e4c0f753799cff1708a6d7682a
Pheenoh <pheenoh@gmail.com>
2026-08-31 18:50:36 -0600

    Decompile 3 more engine.c functions

--- asm/jp/nonmatchings/engine/func_08002BCC.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_08002BCC
-	.thumb
-	.thumb_func
-	.type func_08002BCC, %function
-func_08002BCC:
-	.incbin "roms/B8CJ.gba", 0x2bcc, 0x44
-.syntax divided
