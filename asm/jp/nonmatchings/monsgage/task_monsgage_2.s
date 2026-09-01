4261d2356d3973a62acc45b1ba57d3bbe2634df9
Pheenoh <pheenoh@gmail.com>
2026-08-31 18:53:02 -0600

    Decompile monsgage.c in full

--- asm/jp/nonmatchings/monsgage/task_monsgage_2.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global task_monsgage_2
-	.thumb
-	.thumb_func
-	.type task_monsgage_2, %function
-task_monsgage_2:
-	.incbin "roms/B8CJ.gba", 0x5cb98, 0x84
-.syntax divided
