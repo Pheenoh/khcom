eaada7928c2441ff08d318d9fe83dc82ae70953b
Pheenoh <pheenoh@gmail.com>
2026-09-01 14:38:01 -0600

    engine: decompile four more, 33 -> 29 INCLUDE_ASM

--- asm/jp/nonmatchings/engine/func_08003970.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_08003970
-	.thumb
-	.thumb_func
-	.type func_08003970, %function
-func_08003970:
-	.incbin "roms/B8CJ.gba", 0x3970, 0x100
-.syntax divided
--- asm/jp/nonmatchings/engine/func_0800685C.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_0800685C
-	.thumb
-	.thumb_func
-	.type func_0800685C, %function
-func_0800685C:
-	.incbin "roms/B8CJ.gba", 0x685c, 0xf8
-.syntax divided
