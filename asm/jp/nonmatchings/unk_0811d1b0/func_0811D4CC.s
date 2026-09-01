a6963ec8d86d10cbb8d3078f62ec59dbcc5ea6ad
Pheenoh <pheenoh@gmail.com>
2026-08-31 23:06:20 -0600

    Decompile unk_0811d1b0.c in full; rename it snd_stream.c

--- asm/jp/nonmatchings/unk_0811d1b0/func_0811D4CC.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_0811D4CC
-	.thumb
-	.thumb_func
-	.type func_0811D4CC, %function
-func_0811D4CC:
-	.incbin "roms/B8CJ.gba", 0x11d548, 0x30
-.syntax divided
--- asm/jp/nonmatchings/unk_0811d1b0/func_0811D518.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_0811D518
-	.thumb
-	.thumb_func
-	.type func_0811D518, %function
-func_0811D518:
-	.incbin "roms/B8CJ.gba", 0x11d594, 0x38
-.syntax divided
