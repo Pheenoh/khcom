f13ef5b3388203a6444019cb7271477df4e33e2f
Pheenoh <pheenoh@gmail.com>
2026-09-02 02:27:28 -0600

    Guard the JP-divergent text formatters and their card caller

--- src/card.c
+++ src/card.c
@@ -1561,10 +1561,15 @@ u8 func_0808B30C(u8* work, void* a) {
 
     return 1;
 }
+
+#ifndef VERSION_JP
 void func_0808B398(u8* work) {
     func_08066588(*(s16*)&work[0x894], *(s16*)&work[0x896], &work[0x1E8],
                   *(void**)&work[0x14], 20, work[0x8C6]);
 }
+#else
+INCLUDE_ASM("card/func_0808B398.s");
+#endif
 
 INCLUDE_ASM("card/func_0808B3DC.s");
 
