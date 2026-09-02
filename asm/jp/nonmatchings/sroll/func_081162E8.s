c78122eeb41733fb1cd833c4c6b5e2347777918b
Pheenoh <pheenoh@gmail.com>
2026-09-02 14:29:04 -0600

    Regenerate the JP chunks for sroll

--- asm/jp/nonmatchings/sroll/func_081162E8.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_081162E8
-	.thumb
-	.thumb_func
-	.type func_081162E8, %function
-func_081162E8:
-	.incbin "roms/B8CJ.gba", 0x116364, 0xe4
-.syntax divided
--- asm/jp/nonmatchings/sroll/func_08116644.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_08116644
-	.thumb
-	.thumb_func
-	.type func_08116644, %function
-func_08116644:
-	.incbin "roms/B8CJ.gba", 0x1166c0, 0x54
-.syntax divided
