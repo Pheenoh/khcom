c8936b39d9f68a13528f89ba74a7641e3521cd12
Pheenoh <pheenoh@gmail.com>
2026-09-02 02:13:08 -0600

    Drop the JP chunks for the matched vs-battle functions

--- asm/jp/nonmatchings/mode_vsbattle/func_0800DF30.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_0800DF30
-	.thumb
-	.thumb_func
-	.type func_0800DF30, %function
-func_0800DF30:
-	.incbin "roms/B8CJ.gba", 0xdf30, 0x1a0
-.syntax divided
