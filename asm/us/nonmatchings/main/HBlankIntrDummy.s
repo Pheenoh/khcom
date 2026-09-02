b6835f708b7510ccdd8087f3eb4d817ed69a5ead
Pheenoh <pheenoh@gmail.com>
2026-09-01 21:21:45 -0600

    Decompile the interrupt handlers and heap accessors in main

--- asm/us/nonmatchings/main/HBlankIntrDummy.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global HBlankIntrDummy
-	.thumb
-	.thumb_func
-	.type HBlankIntrDummy, %function
-HBlankIntrDummy: @ 080005A4
-	.byte 0x70, 0x47, 0x00, 0x00
-.syntax divided
