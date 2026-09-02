b014aa21629f80dee6fa65e08680748e4ce18ff5
Pheenoh <pheenoh@gmail.com>
2026-09-02 14:30:13 -0600

    Regenerate the JP chunks for the unit tails

--- asm/jp/nonmatchings/save/SaveWriteFileSmall.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global SaveWriteFileSmall
-	.thumb
-	.thumb_func
-	.type SaveWriteFileSmall, %function
-SaveWriteFileSmall:
-	.incbin "roms/B8CJ.gba", 0x963c, 0x98
-.syntax divided
