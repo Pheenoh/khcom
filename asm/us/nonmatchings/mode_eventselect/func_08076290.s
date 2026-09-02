3273c197e1700b76e7d711d1ac98e4dc45d06d4d
Pheenoh <pheenoh@gmail.com>
2026-09-02 03:28:19 -0600

    Decompile the event-select mode and card flag setters

--- asm/us/nonmatchings/mode_eventselect/func_08076290.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_08076290
-	.thumb
-	.thumb_func
-	.type func_08076290, %function
-func_08076290: @ 08076290
-	.byte 0x01, 0x49, 0x0F, 0x20, 0x08, 0x60, 0x70, 0x47, 0xA0, 0x4A, 0x03, 0x02
-.syntax divided
--- src/mode_eventselect.c
+++ src/mode_eventselect.c
@@ -1,22 +1,140 @@
 #include "macros.h"
+#include "mode_eventselect.h"
 
 INCLUDE_ASM("mode_eventselect/mode_eventselect_0.s");
 INCLUDE_ASM("mode_eventselect/mode_eventselect_1.s");
 INCLUDE_ASM("mode_eventselect/mode_eventselect_2.s");
 INCLUDE_ASM("mode_eventselect/func_08076110.s");
-INCLUDE_ASM("mode_eventselect/func_08076284.s");
-INCLUDE_ASM("mode_eventselect/func_08076290.s");
-INCLUDE_ASM("mode_eventselect/func_0807629C.s");
-INCLUDE_ASM("mode_eventselect/func_08076318.s");
-INCLUDE_ASM("mode_eventselect/func_08076324.s");
-INCLUDE_ASM("mode_eventselect/func_08076330.s");
-INCLUDE_ASM("mode_eventselect/func_0807633C.s");
-INCLUDE_ASM("mode_eventselect/func_08076348.s");
-INCLUDE_ASM("mode_eventselect/func_08076360.s");
-INCLUDE_ASM("mode_eventselect/func_08076374.s");
-INCLUDE_ASM("mode_eventselect/func_08076394.s");
-INCLUDE_ASM("mode_eventselect/func_080763D0.s");
-INCLUDE_ASM("mode_eventselect/func_080763F0.s");
+
+void func_08076284(void) {
+    gUnk_02034AA0 = 14;
+}
+
+void func_08076290(void) {
+    gUnk_02034AA0 = 15;
+}
+
+void func_0807629C(void) {
+    gUnk_02034AA0 = 16;
+}
+
+u8 func_080762A8(void) {
+    return gUnk_02039DD4->unk_0D2;
+}
+
+void func_080762B8(void) {
+    gUnk_02034AA0 = 17;
+}
+
+void func_080762C4(void) {
+    gUnk_02034AA0 = 18;
+}
+
+void func_080762D0(void) {
+    gUnk_02034AA0 = 19;
+}
+
+void func_080762DC(void) {
+    gUnk_02034AA0 = 21;
+}
+
+void func_080762E8(void) {
+    gUnk_02034AA0 = 22;
+}
+
+void func_080762F4(void) {
+    gUnk_02034AA0 = 23;
+}
+
+void func_08076300(void) {
+    gUnk_02034AA0 = 24;
+}
+
+void func_0807630C(void) {
+    gUnk_02034AA0 = 20;
+}
+
+void func_08076318(void) {
+    gUnk_02034A9C = 1;
+}
+
+void func_08076324(void) {
+    gUnk_02034A9C = 2;
+}
+
+void func_08076330(void) {
+    gUnk_02034A9C = 3;
+}
+
+void func_0807633C(void) {
+    gUnk_02034A9C = 4;
+}
+
+void func_08076348(void) {
+    gUnk_02034A9C = 5;
+}
+
+void func_08076354(void) {
+    gUnk_02034A9C = 8;
+}
+
+void func_08076360(void) {
+    gUnk_02034A9C = 6;
+    func_0807E23C();
+}
+
+void func_08076374(void) {
+    gUnk_02034A9C = 7;
+    func_0807E248();
+}
+
+void func_08076388(void) {
+    gUnk_02034A9C = 9;
+}
+
+void func_08076394(void) {
+    gUnk_02034A9C = 10;
+}
+
+void func_080763A0(void) {
+    gUnk_02034A9C = 11;
+}
+
+void func_080763AC(void) {
+    gUnk_02034A9C = 12;
+}
+
+void func_080763B8(void) {
+    gUnk_02034A9C = 13;
+}
+
+void func_080763C4(void) {
+    gUnk_02034A9C = 0;
+}
+
+u8 func_080763D0(void) {
+    if (gUnk_02034A98 != 0 && (gUnk_02034A98->unk_78 & 0x100000)) {
+        return 1;
+    }
+
+    return 0;
+}
+
+void func_080763F0(void) {
+    if (gUnk_02034A98 != 0) {
+        if ((gUnk_02034A98->unk_78 & 0x01000044) == 0x01000044) {
+            gUnk_02039DD4->unk_0E7 = 1;
+        } else {
+            gUnk_02039DD4->unk_0E7 = 0;
+        }
+    } else {
+        gUnk_02039DD4->unk_0E7 = 0;
+    }
+}
+
+void func_08076438(void) {
+}
+
 INCLUDE_ASM("mode_eventselect/func_0807643C.s");
 INCLUDE_ASM("mode_eventselect/func_08076674.s");
 INCLUDE_ASM("mode_eventselect/func_08076750.s");
