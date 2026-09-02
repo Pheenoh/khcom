ea4ee44cd1264487feaa6f34c4efdff2a5a86ef8
Pheenoh <pheenoh@gmail.com>
2026-09-02 03:59:35 -0600

    Regenerate the JP chunks for the sprite unit

--- asm/jp/nonmatchings/sprite/func_08001EF8.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_08001EF8
-	.thumb
-	.thumb_func
-	.type func_08001EF8, %function
-func_08001EF8:
-	.incbin "roms/B8CJ.gba", 0x1ef8, 0x28
-.syntax divided
