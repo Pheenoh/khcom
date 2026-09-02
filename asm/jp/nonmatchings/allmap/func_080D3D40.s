65bb2b2d91149354fb5263a791cea83c59229a54
Pheenoh <pheenoh@gmail.com>
2026-09-02 14:30:13 -0600

    Regenerate the JP chunks for the unit tails

--- asm/jp/nonmatchings/allmap/func_080D3D40.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_080D3D40
-	.thumb
-	.thumb_func
-	.type func_080D3D40, %function
-func_080D3D40:
-	.incbin "roms/B8CJ.gba", 0xd3df4, 0x8c
-.syntax divided
