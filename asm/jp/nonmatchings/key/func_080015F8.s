a8b30c9c09b427c4c6d2a2fdf3991d9c6592522b
Pheenoh <pheenoh@gmail.com>
2026-09-01 21:01:10 -0600

    Decompile UpdateKeyState, completing src/key.c

--- asm/jp/nonmatchings/key/func_080015F8.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_080015F8
-	.thumb
-	.thumb_func
-	.type func_080015F8, %function
-func_080015F8:
-	.incbin "roms/B8CJ.gba", 0x15f8, 0x340
-.syntax divided
