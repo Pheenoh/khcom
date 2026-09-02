491370801e45682bb582d60a1ad253311215b14b
Pheenoh <pheenoh@gmail.com>
2026-09-02 04:59:40 -0600

    Regenerate the JP chunks for the Moogle Shop unit

--- asm/jp/nonmatchings/mode_ms/func_08102F30.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_08102F30
-	.thumb
-	.thumb_func
-	.type func_08102F30, %function
-func_08102F30:
-	.incbin "roms/B8CJ.gba", 0x102f50, 0xda8
-.syntax divided
--- asm/jp/nonmatchings/mode_ms/mode_ms_top_1.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global mode_ms_top_1
-	.thumb
-	.thumb_func
-	.type mode_ms_top_1, %function
-mode_ms_top_1:
-	.incbin "roms/B8CJ.gba", 0x102014, 0x50c
-.syntax divided
