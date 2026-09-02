9ebab1ec6c1415cdd609d0360877e8853f8badef
Pheenoh <pheenoh@gmail.com>
2026-09-02 04:09:29 -0600

    Regenerate the JP chunks for the engine functions

--- asm/jp/nonmatchings/engine/func_08002488.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_08002488
-	.thumb
-	.thumb_func
-	.type func_08002488, %function
-func_08002488:
-	.incbin "roms/B8CJ.gba", 0x2488, 0x21c
-.syntax divided
