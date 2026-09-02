fa7442d38665f23dafb4129aad8480f136faa8e9
Pheenoh <pheenoh@gmail.com>
2026-09-01 22:31:44 -0600

    Split the 188 functions hiding in the unk_0806180c chunks

---
+++ asm/us/nonmatchings/unk_0806180c/func_08074CFC.s
@@ -0,0 +1,9 @@
+.syntax unified
+	.align 2, 0
+	.global func_08074CFC
+	.thumb
+	.thumb_func
+	.type func_08074CFC, %function
+func_08074CFC: @ 08074CFC
+	.byte 0x70, 0x47, 0x00, 0x00
+.syntax divided
