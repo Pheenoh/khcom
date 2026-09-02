583fc80acf9e9a2c2ba8c7aa432e06d1d4519956
Pheenoh <pheenoh@gmail.com>
2026-09-01 21:17:46 -0600

    Name the GBA key bits after the shared decomp convention

---
+++ include/gba/keys.h
@@ -0,0 +1,18 @@
+#ifndef GUARD_GBA_KEYS_H
+#define GUARD_GBA_KEYS_H
+
+#define A_BUTTON        0x0001
+#define B_BUTTON        0x0002
+#define SELECT_BUTTON   0x0004
+#define START_BUTTON    0x0008
+#define DPAD_RIGHT      0x0010
+#define DPAD_LEFT       0x0020
+#define DPAD_UP         0x0040
+#define DPAD_DOWN       0x0080
+#define R_BUTTON        0x0100
+#define L_BUTTON        0x0200
+
+#define DPAD_ANY        0x00F0
+#define KEYS_MASK       0x03FF
+
+#endif /* GUARD_GBA_KEYS_H */
