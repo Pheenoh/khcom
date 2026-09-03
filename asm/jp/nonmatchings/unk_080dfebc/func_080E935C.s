ce60b14e601ceddf020184555060812ac3a409e6
Pheenoh <pheenoh@gmail.com>
2026-09-02 20:46:38 -0600

    Regenerate the JP chunks for the field battle entry

--- asm/jp/nonmatchings/unk_080dfebc/func_080E935C.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_080E935C
-	.thumb
-	.thumb_func
-	.type func_080E935C, %function
-func_080E935C:
-	.incbin "roms/B8CJ.gba", 0xe937c, 0x530
-.syntax divided
--- asm/jp/nonmatchings/unk_080dfebc/func_080E9A28.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global func_080E9A28
-	.thumb
-	.thumb_func
-	.type func_080E9A28, %function
-func_080E9A28:
-	.incbin "roms/B8CJ.gba", 0xe9a48, 0x7c0
-.syntax divided
