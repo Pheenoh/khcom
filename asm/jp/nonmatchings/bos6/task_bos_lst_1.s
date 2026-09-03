7ef8b94f21eed14023a44bc1f9ab2a1fec71a70b
Pheenoh <pheenoh@gmail.com>
2026-09-03 03:15:36 -0600

    Match the three parked bos6 functions

--- asm/jp/nonmatchings/bos6/task_bos_lst_1.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global task_bos_lst_1
-	.thumb
-	.thumb_func
-	.type task_bos_lst_1, %function
-task_bos_lst_1:
-	.incbin "roms/B8CJ.gba", 0x10f344, 0x51c
-.syntax divided
--- asm/jp/nonmatchings/bos6/task_bos_pc_acd_2.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global task_bos_pc_acd_2
-	.thumb
-	.thumb_func
-	.type task_bos_pc_acd_2, %function
-task_bos_pc_acd_2:
-	.incbin "roms/B8CJ.gba", 0x10c0b8, 0x260
-.syntax divided
