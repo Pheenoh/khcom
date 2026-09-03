0d55d1052086c6230c72ce72005caf0d69f00e37
Pheenoh <pheenoh@gmail.com>
2026-09-02 22:34:25 -0600

    Match the save file repair functions

--- asm/jp/nonmatchings/save/SaveRepairFileSmall.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-	.align 2, 0
-	.global SaveRepairFileSmall
-	.thumb
-	.thumb_func
-	.type SaveRepairFileSmall, %function
-SaveRepairFileSmall:
-	.incbin "roms/B8CJ.gba", 0x94ec, 0xdc
-.syntax divided
