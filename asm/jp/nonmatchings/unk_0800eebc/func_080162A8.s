9d30b86f14dadadb2b1232130b8e34fd31474e2d
Pheenoh <pheenoh@gmail.com>
2026-09-04 04:01:56 -0600

    Decompile ten unk_0801007c functions and split a hidden one

---
+++ asm/jp/nonmatchings/unk_0800eebc/func_080162A8.s
@@ -0,0 +1,10 @@
+.syntax unified
+	.text
+	.align 2, 0
+	.global func_080162A8
+	.thumb
+	.thumb_func
+	.type func_080162A8, %function
+func_080162A8:
+	.incbin "roms/B8CJ.gba", 0x162a8, 0x78
+.syntax divided
