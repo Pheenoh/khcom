08565b644f888764f498fe4620331e0c5ea9fc7a
Pheenoh <pheenoh@gmail.com>
2026-09-06 13:22:58 -0600

    Decompile eight functions in unk_08019050 and park one

---
+++ asm/eu/nonmatchings/unk_08019050/func_080195A8.s
@@ -0,0 +1,10 @@
+.syntax unified
+	.text
+	.align 2, 0
+	.global func_080195A8
+	.thumb
+	.thumb_func
+	.type func_080195A8, %function
+func_080195A8:
+	.incbin "roms/B8CP.gba", 0x1d460, 0x204
+.syntax divided
---
+++ asm/jp/nonmatchings/unk_08019050/func_080195A8.s
@@ -0,0 +1,10 @@
+.syntax unified
+	.text
+	.align 2, 0
+	.global func_080195A8
+	.thumb
+	.thumb_func
+	.type func_080195A8, %function
+func_080195A8:
+	.incbin "roms/B8CJ.gba", 0x195a8, 0x204
+.syntax divided
