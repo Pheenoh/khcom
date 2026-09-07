8e6bdf052537571c265b8db244b09a363bd84b5c
Pheenoh <pheenoh@gmail.com>
2026-09-07 01:11:11 -0600

    Drop catch-all else-asm on four more EU-absent mode_sio helpers

--- asm/eu/nonmatchings/mode_sio/func_080B1C70.s
+++
@@ -1,8 +0,0 @@
-.syntax unified
-	.text
-	.global func_080B1C70
-	.thumb
-	.thumb_func
-	.type func_080B1C70, %function
-func_080B1C70:
-.syntax divided
--- asm/eu/nonmatchings/mode_sio/func_080B1DDC.s
+++
@@ -1,8 +0,0 @@
-.syntax unified
-	.text
-	.global func_080B1DDC
-	.thumb
-	.thumb_func
-	.type func_080B1DDC, %function
-func_080B1DDC:
-.syntax divided
