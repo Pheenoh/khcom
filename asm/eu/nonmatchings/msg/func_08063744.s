673be7290af9912e99a41386aab039f5bdd9405b
Pheenoh <pheenoh@gmail.com>
2026-09-07 01:30:46 -0600

    Drop catch-all else-asm on four more EU-absent msg helpers

--- asm/eu/nonmatchings/msg/func_08063744.s
+++
@@ -1,8 +0,0 @@
-.syntax unified
-	.text
-	.global func_08063744
-	.thumb
-	.thumb_func
-	.type func_08063744, %function
-func_08063744:
-.syntax divided
--- asm/eu/nonmatchings/msg/func_0806BDB8.s
+++
@@ -1,8 +0,0 @@
-.syntax unified
-	.text
-	.global func_0806BDB8
-	.thumb
-	.thumb_func
-	.type func_0806BDB8, %function
-func_0806BDB8:
-.syntax divided
