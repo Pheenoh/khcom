4f3bed50f362dab41cbd76649f38529f7d1d5b6f
Pheenoh <pheenoh@gmail.com>
2026-09-07 00:31:05 -0600

    Name TitleMenuWork tiles2 palette2 gfx and TitleObj coords

--- src/title.c
+++ src/title.c
@@ -74,9 +74,9 @@ void task_title_logo_0(TitleLogoWork* work) {
 u8 task_title_logo_1(TitleLogoWork* work) {
     if (func_080D6280() && gUnk_02034ECC == 0) {
         work->unk_48 -= 76;
-        work->unk_4C += 6;
-        if (work->unk_4C > 255) {
-            work->unk_4C = 0x100;
+        work->scale += 6;
+        if (work->scale > 255) {
+            work->scale = 0x100;
             work->unk_48 = 0;
             gUnk_02034ECC = 1;
         }
@@ -109,10 +109,10 @@ void task_title_logo_2(TitleLogoWork* work) {
         }
 
         if (i == 1) {
-            if (work->unk_4C == 0) {
+            if (work->scale == 0) {
                 continue;
             }
-            affine = AllocObjAffine(0, 0x100, work->unk_4C, 0);
+            affine = AllocObjAffine(0, 0x100, work->scale, 0);
 
             if (gGameState.flags & 0x200) {
                 y = 86;
@@ -183,14 +183,14 @@ void task_title_obj_0(TitleObjWork* work) {
 
     if (gGameState.flags & 0x200) {
         if (gLanguage == 4 || gLanguage == 2) {
-            work->unk_00[0].unk_10 = 0xB500;
+            work->unk_00[0].x = 0xB500;
         } else {
-            work->unk_00[0].unk_10 = 0xBA00;
+            work->unk_00[0].x = 0xBA00;
         }
-        work->unk_00[0].unk_0C = 0x76;
+        work->unk_00[0].y = 0x76;
     } else {
-        work->unk_00[0].unk_10 = 0x3D00;
-        work->unk_00[0].unk_0C = 0x77;
+        work->unk_00[0].x = 0x3D00;
+        work->unk_00[0].y = 0x77;
     }
     switch (gLanguage) {
     case 0:
@@ -213,9 +213,9 @@ void task_title_obj_0(TitleObjWork* work) {
         break;
     }
     work->unk_00[1].palette = LoadObjPalette(&gUnk_0984A778[t], 0x20);
-    work->unk_00[1].unk_10 = -0x7800;
-    work->unk_00[1].unk_14 = 0x7C00;
-    work->unk_00[1].unk_0C = 0xA0;
+    work->unk_00[1].x = -0x7800;
+    work->unk_00[1].targetX = 0x7C00;
+    work->unk_00[1].y = 0xA0;
     switch (gLanguage) {
     case 0:
         AnimInit(&work->anim, gUnkEu_09F81A1C, gUnkEu_09F81A08);
@@ -247,17 +247,17 @@ void task_title_obj_0(TitleObjWork* work) {
     work->unk_00[0].gfx = gUnk_09EF65E0[0];
 
     if (gGameState.flags & 0x200) {
-        work->unk_00[0].unk_10 = 0xBA00;
-        work->unk_00[0].unk_0C = 0x76;
+        work->unk_00[0].x = 0xBA00;
+        work->unk_00[0].y = 0x76;
     } else {
-        work->unk_00[0].unk_10 = 0x3D00;
-        work->unk_00[0].unk_0C = 0x77;
+        work->unk_00[0].x = 0x3D00;
+        work->unk_00[0].y = 0x77;
     }
     work->unk_00[1].tiles = LoadObjTiles(gUnk_09771666, 0x700);
     work->unk_00[1].palette = LoadObjPalette(&gUnk_0984A778[t], 0x20);
-    work->unk_00[1].unk_10 = -0x7800;
-    work->unk_00[1].unk_14 = 0x7C00;
-    work->unk_00[1].unk_0C = 0xA0;
+    work->unk_00[1].x = -0x7800;
+    work->unk_00[1].targetX = 0x7C00;
+    work->unk_00[1].y = 0xA0;
     AnimInit(&work->anim, gUnk_09EF6604, gUnk_09EF65F0);
     AnimStart(&work->anim, 0, 1);
     work->unk_00[1].gfx = AnimGetGfx(&work->anim);
@@ -265,9 +265,9 @@ void task_title_obj_0(TitleObjWork* work) {
     work->unk_00[2].palette = LoadObjPalette(&gUnk_0984A778[t], 0x20);
     work->unk_00[2].gfx = gUnk_09EF65E8[0];
 #endif
-    work->unk_00[2].unk_10 = 0x15800;
-    work->unk_00[2].unk_14 = 0xB800;
-    work->unk_00[2].unk_0C = 0x91;
+    work->unk_00[2].x = 0x15800;
+    work->unk_00[2].targetX = 0xB800;
+    work->unk_00[2].y = 0x91;
     work->unk_60 = 30;
     gUnk_02034ED0 = 0;
     work->unk_62 = 0;
@@ -278,8 +278,8 @@ u8 task_title_obj_1(TitleObjWork* work) {
         if (work->unk_62 != 0) {
             work->unk_62--;
         } else if (work->unk_60 != 0) {
-            ApproachValue(&work->unk_00[1].unk_10, work->unk_00[1].unk_14, work->unk_60);
-            ApproachValue(&work->unk_00[2].unk_10, work->unk_00[2].unk_14, work->unk_60);
+            ApproachValue(&work->unk_00[1].x, work->unk_00[1].targetX, work->unk_60);
+            ApproachValue(&work->unk_00[2].x, work->unk_00[2].targetX, work->unk_60);
             work->unk_60--;
             if (work->unk_60 == 0) {
                 gUnk_02034ED0 = 1;
@@ -301,7 +301,7 @@ void task_title_obj_2(TitleObjWork* work) {
     work->unk_00[1].gfx = AnimUpdate(&work->anim);
 
     for (i = 0; i < TITLE_OBJ_DRAW_COUNT; i++) {
-        DrawSprite(work->unk_00[i].unk_10 >> 8, work->unk_00[i].unk_0C, work->unk_00[i].gfx,
+        DrawSprite(work->unk_00[i].x >> 8, work->unk_00[i].y, work->unk_00[i].gfx,
                    work->unk_00[i].tiles, work->unk_00[i].palette, 0, 0, i);
     }
 }
@@ -372,76 +372,76 @@ void task_title_menu_0(TitleMenuWork* work, s16* arg) {
     work->palette = LoadObjPalette(gUnk_0984A7F8, 0x20);
     func_080D5978(work->palette->unk_06 + 16, gUnk_0984A7F8, 0x20);
 #ifdef VERSION_EU
-    work->unk_08[0] = LoadObjTiles(gUnkEu_0973F058, 0x280);
+    work->tiles2[0] = LoadObjTiles(gUnkEu_0973F058, 0x280);
     switch (gLanguage) {
     case 0:
-        work->unk_08[1] = LoadObjTiles(gUnkEu_09741E9A, 0xB20);
+        work->tiles2[1] = LoadObjTiles(gUnkEu_09741E9A, 0xB20);
         break;
     case 1:
-        work->unk_08[1] = LoadObjTiles(gUnkEu_09742A74, 0xCE0);
+        work->tiles2[1] = LoadObjTiles(gUnkEu_09742A74, 0xCE0);
         break;
     case 2:
-        work->unk_08[1] = LoadObjTiles(gUnkEu_0974507E, 0xA60);
+        work->tiles2[1] = LoadObjTiles(gUnkEu_0974507E, 0xA60);
         break;
     case 3:
-        work->unk_08[1] = LoadObjTiles(gUnkEu_097445AC, 0xA20);
+        work->tiles2[1] = LoadObjTiles(gUnkEu_097445AC, 0xA20);
         break;
     case 4:
-        work->unk_08[1] = LoadObjTiles(gUnkEu_09743812, 0xCE0);
+        work->tiles2[1] = LoadObjTiles(gUnkEu_09743812, 0xCE0);
         break;
     case 5:
     case 6:
         break;
     }
-    work->unk_08[2] = LoadObjTiles(gUnkEu_09745B92, 0x700);
+    work->tiles2[2] = LoadObjTiles(gUnkEu_09745B92, 0x700);
 #else
-    work->unk_08[0] = LoadObjTiles(gUnk_09771DC0, 0x280);
-    work->unk_08[1] = LoadObjTiles(gUnk_097720F2, 0xB20);
+    work->tiles2[0] = LoadObjTiles(gUnk_09771DC0, 0x280);
+    work->tiles2[1] = LoadObjTiles(gUnk_097720F2, 0xB20);
 #ifdef VERSION_JP
-    work->unk_08[2] = LoadObjTiles(gUnk_09772CC6, 0xE00);
+    work->tiles2[2] = LoadObjTiles(gUnk_09772CC6, 0xE00);
 #else
-    work->unk_08[2] = LoadObjTiles(gUnk_09772CC6, 0x700);
+    work->tiles2[2] = LoadObjTiles(gUnk_09772CC6, 0x700);
 #endif
 #endif
     pal = &gUnk_0984A778[t];
-    work->unk_14[0] = LoadObjPalette(pal, 0x20);
-    work->unk_14[1] = LoadObjPalette(pal, 0x20);
+    work->palette2[0] = LoadObjPalette(pal, 0x20);
+    work->palette2[1] = LoadObjPalette(pal, 0x20);
     pal2 = &gUnk_0984A7B8[t];
-    work->unk_14[2] = LoadObjPalette(pal2, 0x20);
-    func_080D5978(work->unk_14[0]->unk_06 + 16, pal, 0x20);
-    func_080D5978(work->unk_14[2]->unk_06 + 16, pal2, 0x20);
+    work->palette2[2] = LoadObjPalette(pal2, 0x20);
+    func_080D5978(work->palette2[0]->unk_06 + 16, pal, 0x20);
+    func_080D5978(work->palette2[2]->unk_06 + 16, pal2, 0x20);
 #ifdef VERSION_EU
     AnimInit(&work->anim, gUnkEu_09F81A04, gUnk_09EF65E8);
 #else
     AnimInit(&work->anim, gUnk_09EF661C, gUnk_09EF6608);
 #endif
     AnimStart(&work->anim, 0, 1);
-    work->unk_20[0] = AnimGetGfx(&work->anim);
+    work->gfx[0] = AnimGetGfx(&work->anim);
 #ifdef VERSION_EU
     switch (gLanguage) {
     case 0:
-        work->unk_20[1] = gUnkEu_09F81A80[work->unk_44[0]];
+        work->gfx[1] = gUnkEu_09F81A80[work->unk_44[0]];
         break;
     case 1:
-        work->unk_20[1] = gUnkEu_09F81A9C[work->unk_44[0]];
+        work->gfx[1] = gUnkEu_09F81A9C[work->unk_44[0]];
         break;
     case 2:
-        work->unk_20[1] = gUnkEu_09F81AF0[work->unk_44[0]];
+        work->gfx[1] = gUnkEu_09F81AF0[work->unk_44[0]];
         break;
     case 3:
-        work->unk_20[1] = gUnkEu_09F81AD4[work->unk_44[0]];
+        work->gfx[1] = gUnkEu_09F81AD4[work->unk_44[0]];
         break;
     case 4:
-        work->unk_20[1] = gUnkEu_09F81AB8[work->unk_44[0]];
+        work->gfx[1] = gUnkEu_09F81AB8[work->unk_44[0]];
         break;
     case 5:
     case 6:
         break;
     }
-    work->unk_20[2] = gUnkEu_09F81B0C[work->unk_44[0]];
+    work->gfx[2] = gUnkEu_09F81B0C[work->unk_44[0]];
 #else
-    work->unk_20[1] = gUnk_09EF6620[work->unk_44[0]];
-    work->unk_20[2] = gUnk_09EF663C[work->unk_44[0]];
+    work->gfx[1] = gUnk_09EF6620[work->unk_44[0]];
+    work->gfx[2] = gUnk_09EF663C[work->unk_44[0]];
 #endif
     TaskPoolInit(&work->unk_48, 1);
 #ifdef VERSION_EU
@@ -542,9 +542,9 @@ void func_080D6A64(TitleMenuWork* work) {
 #ifdef VERSION_EU
         void** spr = (void**)gUnkEu_09F801D4[gLanguage];
 
-        DrawSprite(work->unk_60, y, spr[i], work->tiles, work->palette, 0, 0x400, i + 100);
+        DrawSprite(work->x, y, spr[i], work->tiles, work->palette, 0, 0x400, i + 100);
 #else
-        DrawSprite(work->unk_60, y, gUnk_09EF6668[i], work->tiles, work->palette, 0, 0x400, i + 100);
+        DrawSprite(work->x, y, gUnk_09EF6668[i], work->tiles, work->palette, 0, 0x400, i + 100);
 #endif
         y += 24;
     }
@@ -556,7 +556,7 @@ void func_080D6A64(TitleMenuWork* work) {
     }
 
     for (i = 0; i < 3; i++) {
-        DrawSprite(work->unk_60, y, work->unk_20[i], work->unk_08[i], work->unk_14[i], 0, 0, i);
+        DrawSprite(work->x, y, work->gfx[i], work->tiles2[i], work->palette2[i], 0, 0, i);
     }
 }
 
@@ -570,16 +570,16 @@ void func_080D6B7C(TitleMenuWork* work) {
 #ifdef VERSION_EU
         void** spr = (void**)gUnkEu_09F801D4[gLanguage];
 
-        DrawSprite(work->unk_60, y, spr[gUnk_096FDCC8[i]], work->tiles, work->palette, 0, 0x400, i + 100);
+        DrawSprite(work->x, y, spr[gUnk_096FDCC8[i]], work->tiles, work->palette, 0, 0x400, i + 100);
 #else
-        DrawSprite(work->unk_60, y, gUnk_09EF6668[gUnk_096FDCC8[i]], work->tiles, work->palette, 0, 0x400, i + 100);
+        DrawSprite(work->x, y, gUnk_09EF6668[gUnk_096FDCC8[i]], work->tiles, work->palette, 0, 0x400, i + 100);
 #endif
         y += 24;
     }
     y = func_080D6908(work->unk_44[0]) * 24 + 16;
 
     for (i = 0; i < 3; i++) {
-        DrawSprite(work->unk_60, y, work->unk_20[i], work->unk_08[i], work->unk_14[i], 0, 0, i);
+        DrawSprite(work->x, y, work->gfx[i], work->tiles2[i], work->palette2[i], 0, 0, i);
     }
 }
 
@@ -593,16 +593,16 @@ void func_080D6C54(TitleMenuWork* work) {
 #ifdef VERSION_EU
         void** spr = (void**)gUnkEu_09F801D4[gLanguage];
 
-        DrawSprite(work->unk_60, y, spr[gUnk_096FDCC8[i]], work->tiles, work->palette, 0, 0x400, i + 100);
+        DrawSprite(work->x, y, spr[gUnk_096FDCC8[i]], work->tiles, work->palette, 0, 0x400, i + 100);
 #else
-        DrawSprite(work->unk_60, y, gUnk_09EF6668[gUnk_096FDCC8[i]], work->tiles, work->palette, 0, 0x400, i + 100);
+        DrawSprite(work->x, y, gUnk_09EF6668[gUnk_096FDCC8[i]], work->tiles, work->palette, 0, 0x400, i + 100);
 #endif
         y += 24;
     }
     y = func_080D6908(work->unk_44[0]) * 24 + 48;
 
     for (i = 0; i < 3; i++) {
-        DrawSprite(work->unk_60, y, work->unk_20[i], work->unk_08[i], work->unk_14[i], 0, 0, i);
+        DrawSprite(work->x, y, work->gfx[i], work->tiles2[i], work->palette2[i], 0, 0, i);
     }
 }
 
@@ -615,50 +615,50 @@ void func_080D6D2C(TitleMenuWork* work) {
     {
         void** spr = (void**)gUnkEu_09F801D4[gLanguage];
 
-        DrawSprite(work->unk_60, y, spr[work->unk_44[0]], work->tiles, work->palette, 0, 0x400, 100);
+        DrawSprite(work->x, y, spr[work->unk_44[0]], work->tiles, work->palette, 0, 0x400, 100);
     }
 #else
-    DrawSprite(work->unk_60, y, gUnk_09EF6668[work->unk_44[0]], work->tiles, work->palette, 0, 0x400, 100);
+    DrawSprite(work->x, y, gUnk_09EF6668[work->unk_44[0]], work->tiles, work->palette, 0, 0x400, 100);
 #endif
 
     for (i = 0; i < 3; i++) {
-        DrawSprite(work->unk_60, y, work->unk_20[i], work->unk_08[i], work->unk_14[i], 0, 0, i);
+        DrawSprite(work->x, y, work->gfx[i], work->tiles2[i], work->palette2[i], 0, 0, i);
     }
 }
 
 void task_title_menu_2(TitleMenuWork* work) {
-    work->unk_20[0] = AnimUpdate(&work->anim);
+    work->gfx[0] = AnimUpdate(&work->anim);
 #ifdef VERSION_EU
     switch (gLanguage) {
     case 0:
-        work->unk_20[1] = gUnkEu_09F81A80[work->unk_44[0]];
+        work->gfx[1] = gUnkEu_09F81A80[work->unk_44[0]];
         break;
     case 1:
-        work->unk_20[1] = gUnkEu_09F81A9C[work->unk_44[0]];
+        work->gfx[1] = gUnkEu_09F81A9C[work->unk_44[0]];
         break;
     case 2:
-        work->unk_20[1] = gUnkEu_09F81AF0[work->unk_44[0]];
+        work->gfx[1] = gUnkEu_09F81AF0[work->unk_44[0]];
         break;
     case 3:
-        work->unk_20[1] = gUnkEu_09F81AD4[work->unk_44[0]];
+        work->gfx[1] = gUnkEu_09F81AD4[work->unk_44[0]];
         break;
     case 4:
-        work->unk_20[1] = gUnkEu_09F81AB8[work->unk_44[0]];
+        work->gfx[1] = gUnkEu_09F81AB8[work->unk_44[0]];
         break;
     case 5:
     case 6:
         break;
     }
-    work->unk_20[2] = gUnkEu_09F81B0C[work->unk_44[0]];
+    work->gfx[2] = gUnkEu_09F81B0C[work->unk_44[0]];
 #else
-    work->unk_20[1] = gUnk_09EF6620[work->unk_44[0]];
-    work->unk_20[2] = gUnk_09EF663C[work->unk_44[0]];
+    work->gfx[1] = gUnk_09EF6620[work->unk_44[0]];
+    work->gfx[2] = gUnk_09EF663C[work->unk_44[0]];
 #endif
 
     if (gGameState.flags & 0x200) {
-        work->unk_60 = 120;
+        work->x = 120;
     } else {
-        work->unk_60 = 0;
+        work->x = 0;
     }
 
     if (work->unk_5C == 0) {
@@ -680,8 +680,8 @@ void task_title_menu_3(TitleMenuWork* work) {
     ReleaseObjPalette(work->palette);
 
     for (i = 0; i < 3; i++) {
-        ReleaseObjTiles(work->unk_08[i]);
-        ReleaseObjPalette(work->unk_14[i]);
+        ReleaseObjTiles(work->tiles2[i]);
+        ReleaseObjPalette(work->palette2[i]);
     }
     TaskPoolDestroy(&work->unk_48);
 }
