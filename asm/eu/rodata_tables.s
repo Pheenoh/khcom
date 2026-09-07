5ba23924d17c1f6c17379da080d739eb4c8365cb
Pheenoh <pheenoh@gmail.com>
2026-09-07 11:17:19 -0600

    Label the interworking veneers so C can call them directly

--- asm/eu/rodata_tables.s
+++ asm/eu/rodata_tables.s
@@ -1,4 +1,15 @@
 	.section .rodata
 	.global data_0811FEE4
 data_0811FEE4:
-	.incbin "roms/B8CP.gba", 0x11fee4, 0x18
+	.thumb_func
+	.global func_081213C4
+func_081213C4:
+	.incbin "roms/B8CP.gba", 0x11fee4, 0x8
+	.thumb_func
+	.global func_081213CC
+func_081213CC:
+	.incbin "roms/B8CP.gba", 0x11feec, 0x8
+	.thumb_func
+	.global func_081213D4
+func_081213D4:
+	.incbin "roms/B8CP.gba", 0x11fef4, 0x8
--- asm/jp/rodata_tables.s
+++ asm/jp/rodata_tables.s
@@ -1,4 +1,15 @@
 	.section .rodata
 	.global data_08121440
 data_08121440:
-	.incbin "roms/B8CJ.gba", 0x121440, 0x18
+	.thumb_func
+	.global func_081213C4
+func_081213C4:
+	.incbin "roms/B8CJ.gba", 0x121440, 0x8
+	.thumb_func
+	.global func_081213CC
+func_081213CC:
+	.incbin "roms/B8CJ.gba", 0x121448, 0x8
+	.thumb_func
+	.global func_081213D4
+func_081213D4:
+	.incbin "roms/B8CJ.gba", 0x121450, 0x8
