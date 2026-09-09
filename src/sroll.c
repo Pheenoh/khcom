1040510c67dfa4d5770108a0dd5d021a98b521d1
Pheenoh <pheenoh@gmail.com>
2026-09-09 04:11:21 -0500

    Decompile func_08115D60

--- src/sroll.c
+++ src/sroll.c
@@ -1033,32 +1033,26 @@ void func_08115CAC(u32* dst, u16* src, u32* pal, s32 x) {
         dst[i + 8] |= v >> t->unk_04;
     }
 }
-#ifdef NON_MATCHING
 void func_08115D60(u32* dst, u16* src, u32* pal, s32 x) {
     SrollShift* t;
     s32 i;
     u32 v;
-    u32 c;
 
     t = &gUnk_09A54C78[x];
 
     for (i = 0; i <= 7; i++) {
-        c = src[i] << 16;
-        v = pal[(c >> 22) & 3];
-        v |= pal[(c >> 20) & 3] << 4;
-        v |= pal[(c >> 18) & 3] << 8;
+        v = pal[(src[i] >> 6) & 3];
+        v |= pal[(src[i] >> 4) & 3] << 4;
+        v |= pal[(src[i] >> 2) & 3] << 8;
         v |= pal[src[i] & 3] << 12;
-        v |= pal[(c >> 30) & 3] << 16;
-        v |= pal[(c >> 28) & 3] << 20;
-        v |= pal[(c >> 26) & 3] << 24;
-        v |= pal[(c >> 24) & 3] << 28;
+        v |= pal[(src[i] >> 14) & 3] << 16;
+        v |= pal[(src[i] >> 12) & 3] << 20;
+        v |= pal[(src[i] >> 10) & 3] << 24;
+        v |= pal[(src[i] >> 8) & 3] << 28;
         dst[i] |= v << t->unk_00;
         dst[i + 8] |= v >> t->unk_04;
     }
 }
-#else
-INCLUDE_ASM("sroll/func_08115D60.s");
-#endif
 u32 func_08115E24(SrollBlit* w) {
     SrollMask* m;
     u32* d;
