7a6fbbf33a062f17fe8d7c2d51b66967f044a0e0
Pheenoh <pheenoh@gmail.com>
2026-09-09 07:56:05 -0500

    Decompile eu_080059D4

--- asm/eu/nonmatchings/engine/eu_080059D4.s
+++ asm/eu/nonmatchings/engine/eu_080059D4.s
@@ -1,12 +1,6 @@
 .syntax unified
 	.text
 	.align 2, 0
-	.global eu_080059D4
-	.thumb
-	.thumb_func
-	.type eu_080059D4, %function
-eu_080059D4:
-	.incbin "roms/B8CP.gba", 0x59d4, 0x20
 	.align 2, 0
 	.global eu_080059F4
 	.thumb
