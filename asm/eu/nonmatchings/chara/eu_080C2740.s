a1df78ae6f02d9a1c8f0d5b5c89bcaeea55292dd
Pheenoh <pheenoh@gmail.com>
2026-09-09 08:35:57 -0500

    Decompile eu_080C273C

--- asm/eu/nonmatchings/chara/eu_080C2740.s
+++ asm/eu/nonmatchings/chara/eu_080C2740.s
@@ -1,13 +1,6 @@
 .syntax unified
 	.text
 	.align 2, 0
-	.global eu_080C273C
-	.thumb
-	.thumb_func
-	.type eu_080C273C, %function
-eu_080C273C:
-	.incbin "roms/B8CP.gba", 0xc273c, 0x4
-	.align 2, 0
 	.global eu_080C2740
 	.thumb
 	.thumb_func
