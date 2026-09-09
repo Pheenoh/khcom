d9ba39a1905dca76b64fb096009150abfb8c7bef
Pheenoh <pheenoh@gmail.com>
2026-09-09 03:49:17 -0500

    Decompile eu_080DA848

--- asm/eu/nonmatchings/bos4/eu_080DA80C.s
+++ asm/eu/nonmatchings/bos4/eu_080DA80C.s
@@ -1,13 +1,6 @@
 .syntax unified
 	.text
 	.align 2, 0
-	.global eu_080DA848
-	.thumb
-	.thumb_func
-	.type eu_080DA848, %function
-eu_080DA848:
-	.incbin "roms/B8CP.gba", 0xda848, 0x18
-	.align 2, 0
 	.global eu_080DA860
 	.thumb
 	.thumb_func
