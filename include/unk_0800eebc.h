2c746be2c7ec90b98c81d204c11489043c846fc5
Pheenoh <pheenoh@gmail.com>
2026-09-09 09:08:21 -0500

    Decompile func_080195A8

--- include/unk_0800eebc.h
+++ include/unk_0800eebc.h
@@ -656,6 +656,7 @@ void func_08014790(u8 bit);
 void func_080147C8(s32 a, s32 b);
 void func_08019050(u16 a, s32 b, s32 c, s32 d);
 void func_0800FDD0(s32 a);
+u8 func_0807E29C(void);
 void func_0809B644(void* p, s16 x, s16 y, s16 z, u8 n);
 void func_0800F368(Actor* p, u16 n);
 u8 func_0800F4C8(BtlObj* p, u16 b);
