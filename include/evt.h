143b86d0be29e757087e974b37552fedd74e0312
Pheenoh <pheenoh@gmail.com>
2026-09-12 12:51:21 -0600

    Move event object helpers into event translation unit

--- include/evt.h
+++ include/evt.h
@@ -30,6 +30,24 @@ typedef struct EvtObjRes {
     void* unk_08;
 } EvtObjRes;
 
+typedef struct EvtAnimDef {
+    void* unk_00;
+    void* unk_04;
+    void* unk_08;
+} EvtAnimDef;
+
+typedef struct EvtObjAnim {
+    EvtAnimDef* unk_00;
+    u8 unk_04[0x08];
+    u16 unk_0C;
+    u16 unk_0E;
+} EvtObjAnim;
+
+typedef struct EvtObjResTable {
+    EvtObjRes unk_00;
+    u8 unk_0C[0x04];
+} EvtObjResTable;
+
 typedef struct EvtObjParam {
     EvtObjRes* unk_00;
     EvtObj* unk_04;
@@ -52,12 +70,26 @@ typedef struct EvtShadowWork {
 } EvtShadowWork;
 
 void* AllocObjTiles(u16 a, s32 b);
-void func_0801CE70(EvtObjWork* work);
+
+extern EvtObjAnim gUnk_0813766C[];
+extern EvtObjResTable gUnk_0813B09C[];
+extern u8 gTaskDescEvtObj[];
+
+void func_08002A10(void* a, void* b);
+void func_08005974(void* a, u16 b, u16 c, void* d, void* e);
 
 extern u8 gUnk_08B22CBC[];
 extern u8 gUnk_08B22CE4[];
 extern u8 gUnk_08F69BE4[];
 extern u8 gTaskDescEvtShadow[];
+
+void func_0801CD74(EvtObj* obj, s32 anim);
+void EvtObjSetPos(EvtObj* obj, s32 a, s32 b, s32 c);
+void func_0801CD94(EvtObj* obj, s32 a);
+void func_0801CD98(void* pool, EvtObj* obj, s32 res, s32 anim, s32 a, s32 b, s32 c);
+void func_0801CE00(EvtObj* obj, u16 a);
+s32 func_0801CE04(void* pool, void* desc, EvtObj* obj, s32 res, s32 anim, s32 a, s32 b, s32 c);
+void func_0801CE70(EvtObjWork* work);
 
 void task_evt_obj_0(EvtObjWork* work, EvtObjParam* param);
 s32 task_evt_obj_1(EvtObjWork* work);
