9ee46121251cca982797e49bf93856d4698d81ba
Pheenoh <pheenoh@gmail.com>
2026-09-20 14:35:37 +0900

    Recover native SIO debug label ownership

--- include/debug_menu_data.h
+++
@@ -1,7 +0,0 @@
-#ifndef GUARD_DEBUG_MENU_DATA_H
-#define GUARD_DEBUG_MENU_DATA_H
-
-extern const char* const gUnk_09EF34F0[];
-extern const char* const gUnk_09EF3504[];
-
-#endif
--- src/mode_sio_dbg.c
+++ src/mode_sio_dbg.c
@@ -1,7 +1,7 @@
 #include "registration_data.h"
 #include "mode_sio_dbg.h"
 #include "mode_chkbtl.h"
-#include "debug_menu_data.h"
+#include "debug_menu_assets.h"
 #include "sio_debug_state.h"
 #include "sio_battle_options_state.h"
 #include "card_ui_state.h"
@@ -18,6 +18,54 @@ extern const char gUnk_0961A9CC[];
 extern const char gUnk_0961A9E8[];
 
 const char gModeNameSioDbgFlg[] = "mode_sio_dbg_flg";
+
+const char* gUnk_09EF34F0[] = {
+#if defined(VERSION_US)
+    gDebugMenuTextUs_0961A994,
+    gDebugMenuTextUs_0961A980,
+    gDebugMenuTextUs_0961A96C,
+    gDebugMenuTextUs_0961A958,
+    gDebugMenuTextUs_0961A944,
+#elif defined(VERSION_JP)
+    gDebugMenuTextJp_095D3470,
+    gDebugMenuTextJp_095D345C,
+    gDebugMenuTextJp_095D3448,
+    gDebugMenuTextJp_095D3434,
+    gDebugMenuTextJp_095D3420,
+#elif defined(VERSION_EU)
+    gDebugMenuTextEu_095DBE8C,
+    gDebugMenuTextEu_095DBE70,
+    gDebugMenuTextEu_095DBE54,
+    gDebugMenuTextEu_095DBE38,
+    gDebugMenuTextEu_095DBE1C,
+    gDebugMenuTextEu_095DBE00,
+    gDebugMenuTextEu_095DBDE4,
+    gDebugMenuTextEu_095DBDC8,
+    gDebugMenuTextEu_095DBDAC,
+    gDebugMenuTextEu_095DBD90,
+#endif
+};
+
+#ifndef VERSION_EU
+const char* gUnk_09EF3504[] = {
+#if defined(VERSION_US)
+    gDebugMenuTextUs_0961A9B8,
+    gDebugMenuTextUs_0961A9A8,
+#elif defined(VERSION_JP)
+    gDebugMenuTextJp_095D3494,
+    gDebugMenuTextJp_095D3484,
+#endif
+};
+
+#endif
+
+#ifdef VERSION_EU
+const char* gSioUnusedFlagLabelEu_09F7E750 = gDebugMenuTextEu_095DBD74;
+#endif
+
+#ifdef VERSION_EU
+const char* gSioUnusedFlagLabelEu_09F7E754 = gDebugMenuTextEu_095DBD58;
+#endif
 
 Mode gModeSioDbgFlg = {
     gModeNameSioDbgFlg,
