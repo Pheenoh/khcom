e43897d76871ba9b5ab84bc1378ff1d562c345a0
Pheenoh <pheenoh@gmail.com>
2026-09-20 20:13:30 +0900

    Separate map task callbacks and their enemy data from the engine

--- include/map_enemy_data.h
+++ include/map_enemy_data.h
@@ -5,30 +5,13 @@
 
 struct UnkStruct_0984BC9C;
 
-extern struct UnkStruct_0984BC9C* gUnk_09EF83F8[7];
-extern struct UnkStruct_0984BC9C gUnk_0984BC9C;
-extern AnimDef gUnk_0984BB9C[10];
-extern AnimDef gUnk_0984BC3C;
-extern AnimDef gUnk_0984BC4C;
-extern AnimDef gUnk_0984BC5C;
-extern AnimDef gUnk_0984BC6C;
-extern AnimDef gUnk_0984BC7C;
-extern AnimDef gUnk_0984BC8C;
-extern struct UnkStruct_0984BC9C gUnk_0984BD24;
-extern AnimDef gUnk_0984BCC4[6];
-extern struct UnkStruct_0984BC9C gUnk_0984BD8C;
-extern AnimDef gUnk_0984BD4C[2];
-extern AnimDef gUnk_0984BD6C;
-extern AnimDef gUnk_0984BD7C;
-extern struct UnkStruct_0984BC9C gUnk_0984BE14;
-extern AnimDef gUnk_0984BDB4[4];
-extern AnimDef gUnk_0984BDF4;
-extern AnimDef gUnk_0984BE04;
-extern struct UnkStruct_0984BC9C gUnk_0984BE9C;
-extern AnimDef gUnk_0984BE3C[6];
-extern struct UnkStruct_0984BC9C gUnk_0984BF44;
-extern AnimDef gUnk_0984BEC4[8];
-extern struct UnkStruct_0984BC9C gUnk_0984BFEC;
-extern AnimDef gUnk_0984BF6C[8];
+extern const struct UnkStruct_0984BC9C* gUnk_09EF83F8[7];
+extern const struct UnkStruct_0984BC9C gUnk_0984BC9C;
+extern const struct UnkStruct_0984BC9C gUnk_0984BD24;
+extern const struct UnkStruct_0984BC9C gUnk_0984BD8C;
+extern const struct UnkStruct_0984BC9C gUnk_0984BE14;
+extern const struct UnkStruct_0984BC9C gUnk_0984BE9C;
+extern const struct UnkStruct_0984BC9C gUnk_0984BF44;
+extern const struct UnkStruct_0984BC9C gUnk_0984BFEC;
 
 #endif
