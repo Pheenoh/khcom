54595f65fccfb14115cc7331f7c08590c83870a6
Pheenoh <pheenoh@gmail.com>
2026-09-20 14:05:02 +0900

    Synchronize generated EU spans with native SIO ownership

--- asm/eu/asset_gfx_gap_38_at_09041E7F.s
+++ asm/eu/asset_gfx_gap_38_at_09041E7F.s
@@ -1,4 +1,4 @@
 	.section .rodata
 	.global data_090D76FB
 data_090D76FB:
-	.incbin "assets/eu/090D76FB-0954C2C9.bin"
+	.incbin "assets/eu/090D76FB-095DA818.bin"
--- asm/eu/asset_gfx_gap_44.s
+++
@@ -1,4 +0,0 @@
-	.section .rodata
-	.global data_0954C2C9
-data_0954C2C9:
-	.incbin "assets/eu/0954C2C9-095DA818.bin"
