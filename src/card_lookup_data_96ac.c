69dba402c1f8bdee9a0b3157a1d9814e181066a6
Pheenoh <pheenoh@gmail.com>
2026-09-21 17:05:59 +0900

    Use native data for card lookup 96AC pointer cluster

---
+++ src/card_lookup_data_96ac.c
@@ -0,0 +1,746 @@
+#include "card_lookup_data.h"
+#include "card_lookup_assets.h"
+
+void* gUnk_09EE96AC = gUnk_09059E94;
+
+AnimHeader* gUnk_09EE96B0 = &gUnk_09059EA4;
+
+void* gUnk_09EE96B4 = gUnk_0905A130;
+
+AnimHeader* gUnk_09EE96B8 = &gUnk_0905A140;
+
+void* gUnk_09EE96BC = gUnk_0905A3CC;
+
+AnimHeader* gUnk_09EE96C0 = &gUnk_0905A3DC;
+
+void* gUnk_09EE96C4 = gUnk_0905A668;
+
+AnimHeader* gUnk_09EE96C8 = &gUnk_0905A678;
+
+void* gUnk_09EE96CC = gUnk_0905A904;
+
+AnimHeader* gUnk_09EE96D0 = &gUnk_0905A914;
+
+void* gUnk_09EE96D4[1] = {
+    gUnk_0905ABA0,
+};
+
+void* gUnk_09EE96D8 = gUnk_0905ABB0;
+
+void* gUnk_09EE96DC = gUnk_0905ABBA;
+
+void* gUnk_09EE96E0 = gUnk_0905ABC4;
+
+void* gUnk_09EE96E4 = gUnk_0905ABCE;
+
+void* gUnk_09EE96E8 = gUnk_0905ABD8;
+
+void* gUnk_09EE96EC = gUnk_0905ABE2;
+
+void* gUnk_09EE96F0 = gUnk_0905ABEC;
+
+void* gUnk_09EE96F4 = gUnk_0905ABF6;
+
+void* gUnk_09EE96F8 = gUnk_0905AC00;
+
+void* gUnk_09EE96FC = gUnk_0905AC0A;
+
+void* gUnk_09EE9700 = gUnk_0905AC14;
+
+void* gUnk_09EE9704 = gUnk_0905AC1E;
+
+AnimHeader* gUnk_09EE9708 = &gUnk_0905AC28;
+
+AnimHeader* gUnk_09EE970C = &gUnk_0905AC32;
+
+AnimHeader* gUnk_09EE9710 = &gUnk_0905AC48;
+
+AnimHeader* gUnk_09EE9714 = &gUnk_0905AC5E;
+
+AnimHeader* gUnk_09EE9718 = &gUnk_0905AC74;
+
+void* gUnk_09EE971C = gUnk_0905B98C;
+
+void* gUnk_09EE9720 = gUnk_0905B99C;
+
+void* gUnk_09EE9724 = gUnk_0905B9A6;
+
+void* gUnk_09EE9728 = gUnk_0905B9B0;
+
+void* gUnk_09EE972C = gUnk_0905B9BA;
+
+void* gUnk_09EE9730 = gUnk_0905B9C4;
+
+void* gUnk_09EE9734 = gUnk_0905B9CE;
+
+void* gUnk_09EE9738 = gUnk_0905B9D8;
+
+void* gUnk_09EE973C = gUnk_0905B9E2;
+
+void* gUnk_09EE9740 = gUnk_0905B9EC;
+
+void* gUnk_09EE9744 = gUnk_0905B9F6;
+
+void* gUnk_09EE9748 = gUnk_0905BA00;
+
+void* gUnk_09EE974C = gUnk_0905BA0A;
+
+AnimHeader* gUnk_09EE9750 = &gUnk_0905BA14;
+
+AnimHeader* gUnk_09EE9754 = &gUnk_0905BA1E;
+
+AnimHeader* gUnk_09EE9758 = &gUnk_0905BA34;
+
+AnimHeader* gUnk_09EE975C = &gUnk_0905BA4A;
+
+AnimHeader* gUnk_09EE9760 = &gUnk_0905BA60;
+
+void* gUnk_09EE9764[1] = {
+    gUnk_0905C778,
+};
+
+void* gUnk_09EE9768 = gUnk_0905C788;
+
+void* gUnk_09EE976C = gUnk_0905C792;
+
+void* gUnk_09EE9770 = gUnk_0905C79C;
+
+void* gUnk_09EE9774 = gUnk_0905C7A6;
+
+void* gUnk_09EE9778 = gUnk_0905C7B0;
+
+void* gUnk_09EE977C = gUnk_0905C7BA;
+
+void* gUnk_09EE9780 = gUnk_0905C7C4;
+
+void* gUnk_09EE9784 = gUnk_0905C7CE;
+
+void* gUnk_09EE9788 = gUnk_0905C7D8;
+
+void* gUnk_09EE978C = gUnk_0905C7E2;
+
+void* gUnk_09EE9790 = gUnk_0905C7EC;
+
+void* gUnk_09EE9794 = gUnk_0905C7F6;
+
+AnimHeader* gUnk_09EE9798 = &gUnk_0905C800;
+
+AnimHeader* gUnk_09EE979C = &gUnk_0905C80A;
+
+AnimHeader* gUnk_09EE97A0 = &gUnk_0905C820;
+
+AnimHeader* gUnk_09EE97A4 = &gUnk_0905C836;
+
+AnimHeader* gUnk_09EE97A8 = &gUnk_0905C84C;
+
+void* gUnk_09EE97AC[1] = {
+    gUnk_0905D564,
+};
+
+void* gUnk_09EE97B0 = gUnk_0905D574;
+
+void* gUnk_09EE97B4 = gUnk_0905D57E;
+
+void* gUnk_09EE97B8 = gUnk_0905D588;
+
+void* gUnk_09EE97BC = gUnk_0905D592;
+
+void* gUnk_09EE97C0 = gUnk_0905D59C;
+
+void* gUnk_09EE97C4 = gUnk_0905D5A6;
+
+void* gUnk_09EE97C8 = gUnk_0905D5B0;
+
+void* gUnk_09EE97CC = gUnk_0905D5BA;
+
+void* gUnk_09EE97D0 = gUnk_0905D5C4;
+
+void* gUnk_09EE97D4 = gUnk_0905D5CE;
+
+void* gUnk_09EE97D8 = gUnk_0905D5D8;
+
+void* gUnk_09EE97DC = gUnk_0905D5E2;
+
+AnimHeader* gUnk_09EE97E0 = &gUnk_0905D5EC;
+
+AnimHeader* gUnk_09EE97E4 = &gUnk_0905D5F6;
+
+AnimHeader* gUnk_09EE97E8 = &gUnk_0905D60C;
+
+AnimHeader* gUnk_09EE97EC = &gUnk_0905D622;
+
+AnimHeader* gUnk_09EE97F0 = &gUnk_0905D638;
+
+void* gUnk_09EE97F4[1] = {
+    gUnk_0905E350,
+};
+
+void* gUnk_09EE97F8 = gUnk_0905E360;
+
+void* gUnk_09EE97FC = gUnk_0905E36A;
+
+void* gUnk_09EE9800 = gUnk_0905E374;
+
+void* gUnk_09EE9804 = gUnk_0905E37E;
+
+AnimHeader* gUnk_09EE9808 = &gUnk_0905E388;
+
+AnimHeader* gUnk_09EE980C = &gUnk_0905E392;
+
+AnimHeader* gUnk_09EE9810 = &gUnk_0905E39C;
+
+AnimHeader* gUnk_09EE9814 = &gUnk_0905E3A6;
+
+AnimHeader* gUnk_09EE9818 = &gUnk_0905E3B0;
+
+void* gUnk_09EE981C[10] = {
+    gUnk_0905E9BC,
+    gUnk_0905E9C6,
+    gUnk_0905E9D0,
+    gUnk_0905E9DA,
+    gUnk_0905E9E4,
+    gUnk_0905E9EE,
+    gUnk_0905E9F8,
+    gUnk_0905EA02,
+    gUnk_0905EA0C,
+    gUnk_0905EA16,
+};
+
+void* gUnk_09EE9844 = gUnk_0905EA20;
+
+void* gUnk_09EE9848 = gUnk_0905EA2A;
+
+void* gUnk_09EE984C = gUnk_0905EA34;
+
+void* gUnk_09EE9850 = gUnk_0905EA3E;
+
+void* gUnk_09EE9854 = gUnk_0905EA48;
+
+AnimHeader* gUnk_09EE9858 = &gUnk_0905EA52;
+
+AnimHeader* gUnk_09EE985C = &gUnk_0905EA5C;
+
+AnimHeader* gUnk_09EE9860 = &gUnk_0905EA66;
+
+AnimHeader* gUnk_09EE9864 = &gUnk_0905EA70;
+
+AnimHeader* gUnk_09EE9868 = &gUnk_0905EA7A;
+
+AnimHeader* gUnk_09EE986C = &gUnk_0905EA84;
+
+AnimHeader* gUnk_09EE9870 = &gUnk_0905EA8E;
+
+AnimHeader* gUnk_09EE9874 = &gUnk_0905EA98;
+
+AnimHeader* gUnk_09EE9878 = &gUnk_0905EAA2;
+
+AnimHeader* gUnk_09EE987C = &gUnk_0905EAAC;
+
+AnimHeader* gUnk_09EE9880 = &gUnk_0905EAB6;
+
+AnimHeader* gUnk_09EE9884 = &gUnk_0905EAC0;
+
+AnimHeader* gUnk_09EE9888 = &gUnk_0905EACA;
+
+AnimHeader* gUnk_09EE988C = &gUnk_0905EAD4;
+
+AnimHeader* gUnk_09EE9890 = &gUnk_0905EADE;
+
+void* gUnk_09EE9894[10] = {
+    gUnk_0905ECC8,
+    gUnk_0905ECD2,
+    gUnk_0905ECDC,
+    gUnk_0905ECE6,
+    gUnk_0905ECF0,
+    gUnk_0905ECFA,
+    gUnk_0905ED04,
+    gUnk_0905ED0E,
+    gUnk_0905ED18,
+    gUnk_0905ED22,
+};
+
+AnimHeader* gUnk_09EE98BC = &gUnk_0905ED2C;
+
+void* gUnk_09EE98C0[10] = {
+    gUnk_0905EE78,
+    gUnk_0905EE82,
+    gUnk_0905EE8C,
+    gUnk_0905EE96,
+    gUnk_0905EEA0,
+    gUnk_0905EEAA,
+    gUnk_0905EEB4,
+    gUnk_0905EEBE,
+    gUnk_0905EEC8,
+    gUnk_0905EED2,
+};
+
+AnimHeader* gUnk_09EE98E8 = &gUnk_0905EEDC;
+
+void* gUnk_09EE98EC[1] = {
+    gUnk_0905F028,
+};
+
+AnimHeader* gUnk_09EE98F0 = &gUnk_0905F032;
+
+void* gUnk_09EE98F4 = gUnk_0905F0BC;
+
+AnimHeader* gUnk_09EE98F8 = &gUnk_0905F0C6;
+
+void* gUnk_09EE98FC = gUnk_0905F1D0;
+
+AnimHeader* gUnk_09EE9900 = &gUnk_0905F1DA;
+
+void* gUnk_09EE9904 = gUnk_0905F2E4;
+
+AnimHeader* gUnk_09EE9908 = &gUnk_0905F2EE;
+
+void* gUnk_09EE990C = gUnk_0905F3F8;
+
+AnimHeader* gUnk_09EE9910 = &gUnk_0905F402;
+
+void* gUnk_09EE9914 = gUnk_0905F50C;
+
+AnimHeader* gUnk_09EE9918 = &gUnk_0905F516;
+
+void* gUnk_09EE991C = gUnk_0905F620;
+
+AnimHeader* gUnk_09EE9920 = &gUnk_0905F62A;
+
+void* gUnk_09EE9924 = gUnk_0905F734;
+
+AnimHeader* gUnk_09EE9928 = &gUnk_0905F73E;
+
+void* gUnk_09EE992C = gUnk_0905F848;
+
+AnimHeader* gUnk_09EE9930 = &gUnk_0905F852;
+
+void* gUnk_09EE9934 = gUnk_0905F95C;
+
+AnimHeader* gUnk_09EE9938 = &gUnk_0905F966;
+
+void* gUnk_09EE993C = gUnk_0905FA70;
+
+AnimHeader* gUnk_09EE9940 = &gUnk_0905FA7A;
+
+void* gUnk_09EE9944 = gUnk_0905FB84;
+
+AnimHeader* gUnk_09EE9948 = &gUnk_0905FB8E;
+
+void* gUnk_09EE994C = gUnk_0905FC98;
+
+AnimHeader* gUnk_09EE9950 = &gUnk_0905FCA2;
+
+void* gUnk_09EE9954 = gUnk_0905FDAC;
+
+AnimHeader* gUnk_09EE9958 = &gUnk_0905FDB6;
+
+void* gUnk_09EE995C = gUnk_0905FEC0;
+
+AnimHeader* gUnk_09EE9960 = &gUnk_0905FECA;
+
+void* gUnk_09EE9964 = gUnk_0905FFD4;
+
+AnimHeader* gUnk_09EE9968 = &gUnk_0905FFDE;
+
+void* gUnk_09EE996C = gUnk_090600E8;
+
+AnimHeader* gUnk_09EE9970 = &gUnk_090600F2;
+
+void* gUnk_09EE9974 = gUnk_090601FC;
+
+AnimHeader* gUnk_09EE9978 = &gUnk_09060206;
+
+void* gUnk_09EE997C = gUnk_09060310;
+
+AnimHeader* gUnk_09EE9980 = &gUnk_0906031A;
+
+void* gUnk_09EE9984 = gUnk_09060424;
+
+AnimHeader* gUnk_09EE9988 = &gUnk_0906042E;
+
+void* gUnk_09EE998C = gUnk_09060538;
+
+AnimHeader* gUnk_09EE9990 = &gUnk_09060542;
+
+void* gUnk_09EE9994 = gUnk_0906064C;
+
+AnimHeader* gUnk_09EE9998 = &gUnk_09060656;
+
+void* gUnk_09EE999C = gUnk_09060760;
+
+AnimHeader* gUnk_09EE99A0 = &gUnk_0906076A;
+
+void* gUnk_09EE99A4 = gUnk_09060874;
+
+AnimHeader* gUnk_09EE99A8 = &gUnk_0906087E;
+
+void* gUnk_09EE99AC = gUnk_09060988;
+
+AnimHeader* gUnk_09EE99B0 = &gUnk_09060992;
+
+void* gUnk_09EE99B4 = gUnk_09060A9C;
+
+AnimHeader* gUnk_09EE99B8 = &gUnk_09060AA6;
+
+void* gUnk_09EE99BC = gUnk_09060BB0;
+
+AnimHeader* gUnk_09EE99C0 = &gUnk_09060BBA;
+
+void* gUnk_09EE99C4 = gUnk_09060CC4;
+
+AnimHeader* gUnk_09EE99C8 = &gUnk_09060CCE;
+
+void* gUnk_09EE99CC = gUnk_09060DD8;
+
+AnimHeader* gUnk_09EE99D0 = &gUnk_09060DE2;
+
+void* gUnk_09EE99D4 = gUnk_09060EEC;
+
+AnimHeader* gUnk_09EE99D8 = &gUnk_09060EF6;
+
+void* gUnk_09EE99DC = gUnk_09061000;
+
+AnimHeader* gUnk_09EE99E0 = &gUnk_0906100A;
+
+void* gUnk_09EE99E4 = gUnk_09061114;
+
+AnimHeader* gUnk_09EE99E8 = &gUnk_0906111E;
+
+void* gUnk_09EE99EC = gUnk_09061228;
+
+AnimHeader* gUnk_09EE99F0 = &gUnk_09061232;
+
+void* gUnk_09EE99F4 = gUnk_0906133C;
+
+AnimHeader* gUnk_09EE99F8 = &gUnk_09061346;
+
+void* gUnk_09EE99FC = gUnk_09061450;
+
+AnimHeader* gUnk_09EE9A00 = &gUnk_0906145A;
+
+void* gUnk_09EE9A04 = gUnk_09061564;
+
+AnimHeader* gUnk_09EE9A08 = &gUnk_0906156E;
+
+void* gUnk_09EE9A0C = gUnk_09061678;
+
+AnimHeader* gUnk_09EE9A10 = &gUnk_09061682;
+
+void* gUnk_09EE9A14 = gUnk_0906178C;
+
+AnimHeader* gUnk_09EE9A18 = &gUnk_09061796;
+
+void* gUnk_09EE9A1C = gUnk_090618A0;
+
+AnimHeader* gUnk_09EE9A20 = &gUnk_090618AA;
+
+void* gUnk_09EE9A24 = gUnk_090619B4;
+
+AnimHeader* gUnk_09EE9A28 = &gUnk_090619BE;
+
+void* gUnk_09EE9A2C = gUnk_09061AC8;
+
+AnimHeader* gUnk_09EE9A30 = &gUnk_09061AD2;
+
+void* gUnk_09EE9A34 = gUnk_09061BDC;
+
+void* gUnk_09EE9A38 = gUnk_09061BE6;
+
+AnimHeader* gUnk_09EE9A3C = &gUnk_09061BE8;
+
+void* gUnk_09EE9A40 = gUnk_09061CF4;
+
+AnimHeader* gUnk_09EE9A44 = &gUnk_09061CFE;
+
+void* gUnk_09EE9A48 = gUnk_09061E08;
+
+AnimHeader* gUnk_09EE9A4C = &gUnk_09061E12;
+
+void* gUnk_09EE9A50 = gUnk_09061F1C;
+
+AnimHeader* gUnk_09EE9A54 = &gUnk_09061F26;
+
+void* gUnk_09EE9A58 = gUnk_09062030;
+
+AnimHeader* gUnk_09EE9A5C = &gUnk_0906203A;
+
+void* gUnk_09EE9A60 = gUnk_09062144;
+
+AnimHeader* gUnk_09EE9A64 = &gUnk_0906214E;
+
+void* gUnk_09EE9A68 = gUnk_09062258;
+
+AnimHeader* gUnk_09EE9A6C = &gUnk_09062262;
+
+void* gUnk_09EE9A70 = gUnk_0906236C;
+
+AnimHeader* gUnk_09EE9A74 = &gUnk_09062376;
+
+void* gUnk_09EE9A78 = gUnk_09062480;
+
+AnimHeader* gUnk_09EE9A7C = &gUnk_0906248A;
+
+void* gUnk_09EE9A80 = gUnk_09062594;
+
+AnimHeader* gUnk_09EE9A84 = &gUnk_0906259E;
+
+void* gUnk_09EE9A88 = gUnk_090626A8;
+
+AnimHeader* gUnk_09EE9A8C = &gUnk_090626B2;
+
+void* gUnk_09EE9A90 = gUnk_090627BC;
+
+AnimHeader* gUnk_09EE9A94 = &gUnk_090627C6;
+
+void* gUnk_09EE9A98 = gUnk_090628D0;
+
+AnimHeader* gUnk_09EE9A9C = &gUnk_090628DA;
+
+void* gUnk_09EE9AA0 = gUnk_090629E4;
+
+AnimHeader* gUnk_09EE9AA4 = &gUnk_090629EE;
+
+void* gUnk_09EE9AA8 = gUnk_09062AF8;
+
+AnimHeader* gUnk_09EE9AAC = &gUnk_09062B02;
+
+void* gUnk_09EE9AB0 = gUnk_09062C0C;
+
+AnimHeader* gUnk_09EE9AB4 = &gUnk_09062C16;
+
+void* gUnk_09EE9AB8 = gUnk_09062D20;
+
+AnimHeader* gUnk_09EE9ABC = &gUnk_09062D2A;
+
+void* gUnk_09EE9AC0 = gUnk_09062E34;
+
+AnimHeader* gUnk_09EE9AC4 = &gUnk_09062E3E;
+
+void* gUnk_09EE9AC8 = gUnk_09062F48;
+
+AnimHeader* gUnk_09EE9ACC = &gUnk_09062F52;
+
+void* gUnk_09EE9AD0 = gUnk_0906305C;
+
+AnimHeader* gUnk_09EE9AD4 = &gUnk_09063066;
+
+void* gUnk_09EE9AD8 = gUnk_09063170;
+
+AnimHeader* gUnk_09EE9ADC = &gUnk_0906317A;
+
+void* gUnk_09EE9AE0 = gUnk_09063284;
+
+AnimHeader* gUnk_09EE9AE4 = &gUnk_0906328E;
+
+void* gUnk_09EE9AE8 = gUnk_09063398;
+
+AnimHeader* gUnk_09EE9AEC = &gUnk_090633A2;
+
+void* gUnk_09EE9AF0 = gUnk_090634AC;
+
+AnimHeader* gUnk_09EE9AF4 = &gUnk_090634B6;
+
+void* gUnk_09EE9AF8 = gUnk_090635C0;
+
+AnimHeader* gUnk_09EE9AFC = &gUnk_090635CA;
+
+void* gUnk_09EE9B00 = gUnk_090636D4;
+
+AnimHeader* gUnk_09EE9B04 = &gUnk_090636DE;
+
+void* gUnk_09EE9B08 = gUnk_090637E8;
+
+AnimHeader* gUnk_09EE9B0C = &gUnk_090637F2;
+
+void* gUnk_09EE9B10 = gUnk_090638FC;
+
+AnimHeader* gUnk_09EE9B14 = &gUnk_09063906;
+
+void* gUnk_09EE9B18 = gUnk_09063A10;
+
+AnimHeader* gUnk_09EE9B1C = &gUnk_09063A1A;
+
+void* gUnk_09EE9B20 = gUnk_09063B24;
+
+AnimHeader* gUnk_09EE9B24 = &gUnk_09063B2E;
+
+void* gUnk_09EE9B28 = gUnk_09063C38;
+
+AnimHeader* gUnk_09EE9B2C = &gUnk_09063C42;
+
+void* gUnk_09EE9B30 = gUnk_09063D4C;
+
+AnimHeader* gUnk_09EE9B34 = &gUnk_09063D56;
+
+void* gUnk_09EE9B38 = gUnk_09063E60;
+
+AnimHeader* gUnk_09EE9B3C = &gUnk_09063E6A;
+
+void* gUnk_09EE9B40 = gUnk_09063F74;
+
+AnimHeader* gUnk_09EE9B44 = &gUnk_09063F7E;
+
+void* gUnk_09EE9B48 = gUnk_09064088;
+
+AnimHeader* gUnk_09EE9B4C = &gUnk_09064092;
+
+void* gUnk_09EE9B50 = gUnk_0906419C;
+
+AnimHeader* gUnk_09EE9B54 = &gUnk_090641A6;
+
+void* gUnk_09EE9B58 = gUnk_090642B0;
+
+AnimHeader* gUnk_09EE9B5C = &gUnk_090642BA;
+
+void* gUnk_09EE9B60 = gUnk_090643C4;
+
+AnimHeader* gUnk_09EE9B64 = &gUnk_090643CE;
+
+void* gUnk_09EE9B68 = gUnk_090644D8;
+
+AnimHeader* gUnk_09EE9B6C = &gUnk_090644E2;
+
+void* gUnk_09EE9B70 = gUnk_090645EC;
+
+AnimHeader* gUnk_09EE9B74 = &gUnk_090645F6;
+
+void* gUnk_09EE9B78 = gUnk_09064700;
+
+AnimHeader* gUnk_09EE9B7C = &gUnk_0906470A;
+
+void* gUnk_09EE9B80 = gUnk_09064814;
+
+AnimHeader* gUnk_09EE9B84 = &gUnk_0906481E;
+
+void* gUnk_09EE9B88 = gUnk_09064928;
+
+AnimHeader* gUnk_09EE9B8C = &gUnk_09064932;
+
+void* gUnk_09EE9B90 = gUnk_09064A3C;
+
+AnimHeader* gUnk_09EE9B94 = &gUnk_09064A46;
+
+void* gUnk_09EE9B98 = gUnk_09064B50;
+
+AnimHeader* gUnk_09EE9B9C = &gUnk_09064B5A;
+
+void* gUnk_09EE9BA0 = gUnk_09064C64;
+
+AnimHeader* gUnk_09EE9BA4 = &gUnk_09064C6E;
+
+void* gUnk_09EE9BA8 = gUnk_09064D78;
+
+AnimHeader* gUnk_09EE9BAC = &gUnk_09064D82;
+
+void* gUnk_09EE9BB0 = gUnk_09064E8C;
+
+AnimHeader* gUnk_09EE9BB4 = &gUnk_09064E96;
+
+void* gUnk_09EE9BB8 = gUnk_09064FA0;
+
+AnimHeader* gUnk_09EE9BBC = &gUnk_09064FAA;
+
+void* gUnk_09EE9BC0 = gUnk_090650B4;
+
+AnimHeader* gUnk_09EE9BC4 = &gUnk_090650BE;
+
+void* gUnk_09EE9BC8 = gUnk_090651C8;
+
+AnimHeader* gUnk_09EE9BCC = &gUnk_090651D2;
+
+void* gUnk_09EE9BD0 = gUnk_090652DC;
+
+AnimHeader* gUnk_09EE9BD4 = &gUnk_090652E6;
+
+void* gUnk_09EE9BD8 = gUnk_090653F0;
+
+AnimHeader* gUnk_09EE9BDC = &gUnk_090653FA;
+
+void* gUnk_09EE9BE0 = gUnk_09065504;
+
+AnimHeader* gUnk_09EE9BE4 = &gUnk_0906550E;
+
+void* gUnk_09EE9BE8 = gUnk_09065618;
+
+AnimHeader* gUnk_09EE9BEC = &gUnk_09065622;
+
+void* gUnk_09EE9BF0 = gUnk_0906572C;
+
+AnimHeader* gUnk_09EE9BF4 = &gUnk_09065736;
+
+void* gUnk_09EE9BF8 = gUnk_09065840;
+
+AnimHeader* gUnk_09EE9BFC = &gUnk_0906584A;
+
+void* gUnk_09EE9C00 = gUnk_09065954;
+
+AnimHeader* gUnk_09EE9C04 = &gUnk_0906595E;
+
+void* gUnk_09EE9C08 = gUnk_09065A68;
+
+AnimHeader* gUnk_09EE9C0C = &gUnk_09065A72;
+
+void* gUnk_09EE9C10 = gUnk_09065B7C;
+
+AnimHeader* gUnk_09EE9C14 = &gUnk_09065B86;
+
+void* gUnk_09EE9C18 = gUnk_09065C90;
+
+AnimHeader* gUnk_09EE9C1C = &gUnk_09065C9A;
+
+void* gUnk_09EE9C20 = gUnk_09065DA4;
+
+AnimHeader* gUnk_09EE9C24 = &gUnk_09065DAE;
+
+void* gUnk_09EE9C28 = gUnk_09065EB8;
+
+AnimHeader* gUnk_09EE9C2C = &gUnk_09065EC2;
+
+void* gUnk_09EE9C30 = gUnk_09065FCC;
+
+AnimHeader* gUnk_09EE9C34 = &gUnk_09065FD6;
+
+void* gUnk_09EE9C38 = gUnk_090660E0;
+
+AnimHeader* gUnk_09EE9C3C = &gUnk_090660EA;
+
+void* gUnk_09EE9C40 = gUnk_090661F4;
+
+AnimHeader* gUnk_09EE9C44 = &gUnk_090661FE;
+
+void* gUnk_09EE9C48 = gUnk_09066308;
+
+AnimHeader* gUnk_09EE9C4C = &gUnk_09066312;
+
+void* gUnk_09EE9C50 = gUnk_0906641C;
+
+AnimHeader* gUnk_09EE9C54 = &gUnk_09066426;
+
+void* gUnk_09EE9C58 = gUnk_09066530;
+
+AnimHeader* gUnk_09EE9C5C = &gUnk_0906653A;
+
+void* gUnk_09EE9C60 = gUnk_09066644;
+
+AnimHeader* gUnk_09EE9C64 = &gUnk_0906664E;
+
+void* gUnk_09EE9C68 = gUnk_09066758;
+
+AnimHeader* gUnk_09EE9C6C = &gUnk_09066762;
+
+void* gUnk_09EE9C70 = gUnk_0906686C;
+
+AnimHeader* gUnk_09EE9C74 = &gUnk_09066876;
+
+void* gUnk_09EE9C78 = gUnk_09066980;
+
+AnimHeader* gUnk_09EE9C7C = &gUnk_0906698A;
+
+void* gUnk_09EE9C80 = gUnk_09066A94;
+
+AnimHeader* gUnk_09EE9C84 = &gUnk_09066A9E;
+
+void* gUnk_09EE9C88 = gUnk_09066BA8;
+
+AnimHeader* gUnk_09EE9C8C = &gUnk_09066BB2;
+
