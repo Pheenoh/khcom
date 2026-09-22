72ddc98ed7789aa43f31ccb217f19d5c8a5ae70b
Pheenoh <pheenoh@gmail.com>
2026-09-22 16:16:08 +0900

    Split exclusive mode_test/card/map animation leftovers into native TUs

---
+++ src/animation_resource_map_menu_data.c
@@ -0,0 +1,17 @@
+#include "animation_resource_data.h"
+#include "animation_resource_assets.h"
+
+void* gUnk_09EF8D48[4] = {
+    gUnk_098A859C,
+    gUnk_098A85B2,
+    gUnk_098A85C8,
+    gUnk_098A85DE,
+};
+
+AnimHeader* gUnk_09EF8D58[3] = {
+    &gUnk_098A85F4,
+    &gUnk_098A85FA,
+    &gUnk_098A8600,
+};
+
+AnimHeader* gUnk_09EF8D64 = &gUnk_098A861E;
