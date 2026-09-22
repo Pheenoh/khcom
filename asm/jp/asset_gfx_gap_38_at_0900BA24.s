7d6922f9570aad8b1810cf2d3ee558864039fa4a
Pheenoh <pheenoh@gmail.com>
2026-09-23 01:53:47 +0900

    Define the JP card help text inline in card_help_data.c

--- asm/jp/asset_gfx_gap_38_at_0900BA24.s
+++
@@ -1,4 +0,0 @@
-	.section .rodata
-	.global data_0900BA24
-data_0900BA24:
-	.incbin "assets/jp/0900BA24-0900BA50.bin"
--- asm/jp/asset_gfx_gap_38_at_0900BA58.s
+++
@@ -1,4 +0,0 @@
-	.section .rodata
-	.global data_0900BA58
-data_0900BA58:
-	.incbin "assets/jp/0900BA58-0900D1A4.bin"
