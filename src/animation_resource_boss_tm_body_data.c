075dd3f45fe956082651d5fbd8d03448fe6d30b1
Pheenoh <pheenoh@gmail.com>
2026-09-22 15:54:40 +0900

    Split boss sprite leftovers into native TUs

---
+++ src/animation_resource_boss_tm_body_data.c
@@ -0,0 +1,40 @@
+#include "animation_resource_data.h"
+#include "animation_resource_assets.h"
+
+void* gUnk_09EF3950 = gUnk_0962E6B4;
+
+AnimHeader* gUnk_09EF3954 = &gUnk_0962E6CA;
+
+void* gUnk_09EF3958 = gUnk_0962E6D4;
+
+AnimHeader* gUnk_09EF395C = &gUnk_0962E6EA;
+
+void* gUnk_09EF3960[4] = {
+    gUnk_0962E6F4,
+    gUnk_0962E6FE,
+    gUnk_0962E708,
+    gUnk_0962E712,
+};
+
+void* gUnk_09EF3970 = gUnk_0962E722;
+
+void* gUnk_09EF3974 = gUnk_0962E732;
+
+AnimHeader* gUnk_09EF3978 = &gUnk_0962E73C;
+
+void* gUnk_09EF397C[4] = {
+    gUnk_0962E748,
+    gUnk_0962E758,
+    gUnk_0962E768,
+    gUnk_0962E778,
+};
+
+void* gUnk_09EF398C = gUnk_0962E782;
+
+void* gUnk_09EF3990 = gUnk_0962E78C;
+
+AnimHeader* gUnk_09EF3994 = &gUnk_0962E796;
+
+void* gUnk_09EF3998 = gUnk_0962E7A0;
+
+AnimHeader* gUnk_09EF399C = &gUnk_0962E7AA;
---
+++ src/animation_resource_boss_tm_walk_data.c
@@ -0,0 +1,17 @@
+#include "animation_resource_data.h"
+#include "animation_resource_assets.h"
+
+void* gUnk_09EF3A1C[10] = {
+    gUnk_0962EA44,
+    gUnk_0962EA7E,
+    gUnk_0962EABE,
+    gUnk_0962EAF2,
+    gUnk_0962EB26,
+    gUnk_0962EB54,
+    gUnk_0962EB88,
+    gUnk_0962EBB0,
+    gUnk_0962EBD8,
+    gUnk_0962EC12,
+};
+
+AnimHeader* gUnk_09EF3A44 = &gUnk_0962EC52;
