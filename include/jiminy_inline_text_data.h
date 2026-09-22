60f59d81ea58599f1b2fcb474be563717ec85d32
Pheenoh <pheenoh@gmail.com>
2026-09-22 20:55:59 +0900

    Make the Jiminy inline text const

--- include/jiminy_inline_text_data.h
+++ include/jiminy_inline_text_data.h
@@ -5,60 +5,60 @@
 
 #ifdef VERSION_EU
 
-extern JiminyLocalizedName gUnkEu_0888E364;
-extern JiminyLocalizedName gUnkEu_0888E3A0;
-extern JiminyLocalizedName gUnkEu_0888E410;
-extern JiminyLocalizedName gUnkEu_0888E450;
-extern JiminyLocalizedName gUnkEu_0888E4C0;
+extern const JiminyLocalizedName gUnkEu_0888E364;
+extern const JiminyLocalizedName gUnkEu_0888E3A0;
+extern const JiminyLocalizedName gUnkEu_0888E410;
+extern const JiminyLocalizedName gUnkEu_0888E450;
+extern const JiminyLocalizedName gUnkEu_0888E4C0;
 
-extern JiminyTextChar gUnkEu_0888E310[14];
-extern JiminyTextChar gUnkEu_0888E31E[18];
-extern JiminyTextChar gUnkEu_0888E330[19];
-extern JiminyTextChar gUnkEu_0888E343[18];
-extern JiminyTextChar gUnkEu_0888E355[15];
-extern JiminyTextChar gUnkEu_0888E378[8];
-extern JiminyTextChar gUnkEu_0888E380[8];
-extern JiminyTextChar gUnkEu_0888E388[8];
-extern JiminyTextChar gUnkEu_0888E390[8];
-extern JiminyTextChar gUnkEu_0888E398[8];
-extern JiminyTextChar gUnkEu_0888E3B4[11];
-extern JiminyTextChar gUnkEu_0888E3BF[20];
-extern JiminyTextChar gUnkEu_0888E3D3[11];
-extern JiminyTextChar gUnkEu_0888E3DE[26];
-extern JiminyTextChar gUnkEu_0888E3F8[23];
-extern JiminyTextChar gUnkEu_0888E424[8];
-extern JiminyTextChar gUnkEu_0888E42C[8];
-extern JiminyTextChar gUnkEu_0888E434[8];
-extern JiminyTextChar gUnkEu_0888E43C[10];
-extern JiminyTextChar gUnkEu_0888E446[9];
-extern JiminyTextChar gUnkEu_0888E464[15];
-extern JiminyTextChar gUnkEu_0888E473[19];
-extern JiminyTextChar gUnkEu_0888E486[15];
-extern JiminyTextChar gUnkEu_0888E495[22];
-extern JiminyTextChar gUnkEu_0888E4AB[20];
+extern const JiminyTextChar gUnkEu_0888E310[14];
+extern const JiminyTextChar gUnkEu_0888E31E[18];
+extern const JiminyTextChar gUnkEu_0888E330[19];
+extern const JiminyTextChar gUnkEu_0888E343[18];
+extern const JiminyTextChar gUnkEu_0888E355[15];
+extern const JiminyTextChar gUnkEu_0888E378[8];
+extern const JiminyTextChar gUnkEu_0888E380[8];
+extern const JiminyTextChar gUnkEu_0888E388[8];
+extern const JiminyTextChar gUnkEu_0888E390[8];
+extern const JiminyTextChar gUnkEu_0888E398[8];
+extern const JiminyTextChar gUnkEu_0888E3B4[11];
+extern const JiminyTextChar gUnkEu_0888E3BF[20];
+extern const JiminyTextChar gUnkEu_0888E3D3[11];
+extern const JiminyTextChar gUnkEu_0888E3DE[26];
+extern const JiminyTextChar gUnkEu_0888E3F8[23];
+extern const JiminyTextChar gUnkEu_0888E424[8];
+extern const JiminyTextChar gUnkEu_0888E42C[8];
+extern const JiminyTextChar gUnkEu_0888E434[8];
+extern const JiminyTextChar gUnkEu_0888E43C[10];
+extern const JiminyTextChar gUnkEu_0888E446[9];
+extern const JiminyTextChar gUnkEu_0888E464[15];
+extern const JiminyTextChar gUnkEu_0888E473[19];
+extern const JiminyTextChar gUnkEu_0888E486[15];
+extern const JiminyTextChar gUnkEu_0888E495[22];
+extern const JiminyTextChar gUnkEu_0888E4AB[20];
 
 #endif
 
 #ifdef VERSION_JP
 
-extern JiminyTextChar gUnkJp_0813FEB8[21];
-extern JiminyTextChar gUnkJp_0813FED0[25];
-extern JiminyTextChar gUnkJp_0813FEEC[25];
-extern JiminyTextChar gUnkJp_0813FF08[25];
-extern JiminyTextChar gUnkJp_0813FF24[19];
-extern JiminyTextChar gUnkJp_0813FF38[25];
-extern JiminyTextChar gUnkJp_0813FF54[25];
-extern JiminyTextChar gUnkJp_0813FF70[11];
-extern JiminyTextChar gUnkJp_0813FF7C[25];
-extern JiminyTextChar gUnkJp_0813FF98[25];
-extern JiminyTextChar gUnkJp_0813FFB4[25];
-extern JiminyTextChar gUnkJp_0813FFD0[25];
-extern JiminyTextChar gUnkJp_0813FFEC[9];
-extern JiminyTextChar gUnkJp_0813FFF8[25];
-extern JiminyTextChar gUnkJp_08140014[25];
-extern JiminyTextChar gUnkJp_08140030[25];
-extern JiminyTextChar gUnkJp_0814004C[25];
-extern JiminyTextChar gUnkJp_08140068[25];
+extern const JiminyTextChar gUnkJp_0813FEB8[21];
+extern const JiminyTextChar gUnkJp_0813FED0[25];
+extern const JiminyTextChar gUnkJp_0813FEEC[25];
+extern const JiminyTextChar gUnkJp_0813FF08[25];
+extern const JiminyTextChar gUnkJp_0813FF24[19];
+extern const JiminyTextChar gUnkJp_0813FF38[25];
+extern const JiminyTextChar gUnkJp_0813FF54[25];
+extern const JiminyTextChar gUnkJp_0813FF70[11];
+extern const JiminyTextChar gUnkJp_0813FF7C[25];
+extern const JiminyTextChar gUnkJp_0813FF98[25];
+extern const JiminyTextChar gUnkJp_0813FFB4[25];
+extern const JiminyTextChar gUnkJp_0813FFD0[25];
+extern const JiminyTextChar gUnkJp_0813FFEC[9];
+extern const JiminyTextChar gUnkJp_0813FFF8[25];
+extern const JiminyTextChar gUnkJp_08140014[25];
+extern const JiminyTextChar gUnkJp_08140030[25];
+extern const JiminyTextChar gUnkJp_0814004C[25];
+extern const JiminyTextChar gUnkJp_08140068[25];
 
 #endif
 
