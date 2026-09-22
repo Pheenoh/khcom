c367f840063bd12973e9b015a327a50e16560e7c
Pheenoh <pheenoh@gmail.com>
2026-09-22 15:20:12 +0900

    Split room and worldinspect resources into native TUs

---
+++ src/worldinspect_resources_data.c
@@ -0,0 +1,115 @@
+#include "animation_resource_data.h"
+#include "animation_resource_assets.h"
+#include "room_resource_assets.h"
+
+void* gUnk_09EF9BC4[1] = {
+    gUnk_099A8758,
+};
+
+AnimHeader* gUnk_09EF9BC8[1] = {
+    &gUnk_099A8774,
+};
+
+void* gUnk_09EF9BCC[1] = {
+    gUnk_099A8780,
+};
+
+AnimHeader* gUnk_09EF9BD0[1] = {
+    &gUnk_099A87B4,
+};
+
+void* gUnk_09EF9BD4[1] = {
+    gUnk_099A87C0,
+};
+
+AnimHeader* gUnk_09EF9BD8[1] = {
+    &gUnk_099A87EE,
+};
+
+void* gUnk_09EF9BDC[1] = {
+    gUnk_099A87F8,
+};
+
+AnimHeader* gUnk_09EF9BE0[1] = {
+    &gUnk_099A881A,
+};
+
+void* gUnk_09EF9BE4[1] = {
+    gUnk_099A8824,
+};
+
+AnimHeader* gUnk_09EF9BE8[1] = {
+    &gUnk_099A8840,
+};
+
+void* gUnk_09EF9BEC[1] = {
+    gUnk_099A884C,
+};
+
+AnimHeader* gUnk_09EF9BF0[1] = {
+    &gUnk_099A8874,
+};
+
+void* gUnk_09EF9BF4[1] = {
+    gUnk_099A8880,
+};
+
+AnimHeader* gUnk_09EF9BF8[1] = {
+    &gUnk_099A8896,
+};
+
+void* gUnk_09EF9BFC[1] = {
+    gUnk_099A88A0,
+};
+
+AnimHeader* gUnk_09EF9C00[1] = {
+    &gUnk_099A88C8,
+};
+
+void* gUnk_09EF9C04[1] = {
+    gUnk_099A88D4,
+};
+
+AnimHeader* gUnk_09EF9C08[1] = {
+    &gUnk_099A88F6,
+};
+
+void* gUnk_09EF9C0C[1] = {
+    gUnk_099A8900,
+};
+
+AnimHeader* gUnk_09EF9C10[1] = {
+    &gUnk_099A890A,
+};
+
+void* gUnk_09EF9C14[1] = {
+    gUnk_099A8914,
+};
+
+AnimHeader* gUnk_09EF9C18[1] = {
+    &gUnk_099A8924,
+};
+
+void* gUnk_09EF9C1C[1] = {
+    gUnk_099A8930,
+};
+
+AnimHeader* gUnk_09EF9C20[1] = {
+    &gUnk_099A8952,
+};
+
+void* gUnk_09EF9C24[1] = {
+    gUnk_099A895C,
+};
+
+AnimHeader* gUnk_09EF9C28[1] = {
+    &gUnk_099A8972,
+};
+
+void* gUnk_09EF9C2C[1] = {
+    gUnk_099A897C,
+};
+
+AnimHeader* gUnk_09EF9C30[1] = {
+    &gUnk_099A8992,
+};
